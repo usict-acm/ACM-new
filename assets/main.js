aed36d6e0d97ba476694ca9c710285e2d320d151
Harsh Goyal <harsh.student99@gmail.com>
2020-12-10 02:51:53 +0530

    Announcement section

--- assets/main.js
+++ assets/main.js
@@ -2111,3 +2111,34 @@ window.addEventListener('load', function(){
 
   })
 });
+
+
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demoDay").innerHTML = days + "<br/><div class = countdown>DAYS</div> ";
+  document.getElementById("demoHour").innerHTML = hours + "<br/><div class = countdown>H0URS</div> ";
+  document.getElementById("demoMin").innerHTML = minutes + "<br/><div class = countdown>MINUTES</div> ";
+  document.getElementById("demoSec").innerHTML = seconds + "<br/><div class = countdown>SECONDS</div> ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
