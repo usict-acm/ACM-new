f33f3c490b04b2479dac3e4e86ef7c7e2b8ae0c9
Harsh Goyal <harsh.student99@gmail.com>
2020-12-26 21:39:14 +0530

    Minor tweaks

--- assets/main.js
+++ assets/main.js
@@ -2136,15 +2136,6 @@ var x = setInterval(function() {
   document.getElementById("demoMin").innerHTML = minutes + "<br/><div class = countdown>MIN(s)</div> ";
   document.getElementById("demoSec").innerHTML = seconds + "<br/><div class = countdown>SEC(s)</div> ";
 
-  // If the count down is finished, write some text
-document.getElementById("links").addEventListener("click", function(){
-  if(distance>0)
-    {
-      document.getElementById("links").disabled = true;
-      alert("Meeting not started yet");
-    }
-
-})
 
   if (distance < 0) {
     clearInterval(x);
