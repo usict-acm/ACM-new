9d6dfaf97dbeb73a7b442a250744b514aa133a2d
Tarun <tarunshekhawat66@gmail.com>
2021-02-12 23:46:04 +0530

    registration section

--- assets/JS/register.js
+++ assets/JS/register.js
@@ -1,18 +1,32 @@
-function _class(name){
-    return document.getElementsByClassName(name);
-  }
+// function _class(name){
+//     return document.getElementsByClassName(name);
+//   }
    
-  let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
+//   let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
    
-  for(let i=0;i<tabPanes.length;i++){
-    tabPanes[i].addEventListener("click",function(){
-      _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
-      tabPanes[i].classList.add("active");
+//   for(let i=0;i<tabPanes.length;i++){
+//     tabPanes[i].addEventListener("click",function(){
+//       _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
+//       tabPanes[i].classList.add("active");
       
-      _class("tab-indicator")[0].style.top = `calc(70px + ${i*45}px)`;
+//       _class("tab-indicator")[0].style.top = `calc(70px + ${i*45}px)`;
       
-      _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
-      _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
+//       _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
+//       _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
       
-    });
-  }
+//     });
+//   }
+
+
+
+
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+  $("#example-vertical").steps({
+    headerTag: "h3",
+    bodyTag: "section",
+    transitionEffect: "slideLeft",
+    stepsOrientation: "vertical"
+  });
+});
