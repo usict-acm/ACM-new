e0e561d5687f473e0c3495e566fe840537df2ce8
unknown <anuj.talwar.12@gmail.com>
2021-02-12 15:10:44 +0530

    navbar changes

--- assets/JS/hackathon.js
+++ assets/JS/hackathon.js
@@ -1,9 +1,9 @@
 $(window).on("scroll", function() {
-      if($(window).scrollTop()) {
-            $('nav').addClass('black-bg');
-      }
+           if($(window).scrollTop()) {
+                 $('nav').addClass('black');
+           }
 
-      else {
-            $('nav').removeClass('black-bg');
-      }
-})
+           else {
+                 $('nav').removeClass('black');
+           }
+     })
