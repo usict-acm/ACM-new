299f0e3d97d8267031ce094de74d77c270e4ccc6
Tarun <tarunshekhawat66@gmail.com>
2021-02-15 10:21:18 +0530

    registration section mobile view

--- assets/JS/hack-timer.js
+++ assets/JS/hack-timer.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "Feb 15, 2021 00:00:00",
+  let birthday = "Feb 20, 2021 00:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {
 
