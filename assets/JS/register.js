d961664ff8ee477ec5c85cbb389fd37bb449c27b
Tarun <tarunshekhawat66@gmail.com>
2021-03-03 00:09:59 +0530

    jquery validation

--- assets/JS/register.js
+++ assets/JS/register.js
@@ -17,18 +17,43 @@
 //     });
 //   }
 
+
+
+
+
 $(document).ready(function() {
 
   var count = 0;
   var det = document.getElementById("my-form");
   var db = firebase.firestore();
+ var form = $("#my-form");
 
+ form.validate({
+   errorPlacement: function errorPlacement(error, element) { element.before(error); },
+   rules: {
+       confirm: {
+           equalTo: "#password"
+       }
+   }
+});
   console.log("ready!");
   $("#my-form").steps({
       headerTag: "h3",
       bodyTag: "section",
+      saveState: true,
       transitionEffect: "slideLeft",
       stepsOrientation: "vertical",
+      onStepChanging: function (event, currentIndex, newIndex)
+    {
+        form.validate().settings.ignore = ":disabled,:hidden";
+        return form.valid();
+    },
+    onFinishing: function (event, currentIndex)
+    {
+      var form = $(this);
+        form.validate().settings.ignore = ":disabled";
+        return form.valid();
+    },
       onFinished: function(event, currentIndex) {
         event.preventDefault();
 
@@ -120,125 +145,141 @@ $(document).ready(function() {
 
 
 
-$(function() {
+// $(function() {
 
-  $("#teamName_error_message").hide();
-  $("#member_error_message").hide();
-  $("#leaderName_error_message").hide();
-  $("#leaderNumber_error_message").hide();
-  $("#leaderEmail_error_message").hide();
+//   $("#teamName_error_message").hide();
+//   $("#member_error_message").hide();
+//   $("#leaderName_error_message").hide();
+//   $("#leaderNumber_error_message").hide();
+//   $("#leaderEmail_error_message").hide();
 
-  var error_teamName = false;
-  var error_member = false;
-  var error_leaderName = false;
-  var error_leaderNumber = false;
-  var error_leaderEmail = false;
+//   var error_teamName = false;
+//   var error_member = false;
+//   var error_leaderName = false;
+//   var error_leaderNumber = false;
+//   var error_leaderEmail = false;
 
-  $("#teamName").focusout(function(){
-     check_teamName();
-  });
-  $("#member").focusout(function() {
-     check_member();
-  });
-  $("#leaderName").focusout(function() {
-     check_leaderName();
-  });
-  $("#leaderNumber").focusout(function() {
-     check_leaderNumber();
-  });
-  $("#leaderEmail").focusout(function() {
-     check_leaderEmail();
-  });
-
-  function check_teamName() {
-     var pattern = /^[a-zA-Z]*$/;
-     var teamName = $("#teamName").val();
-     if (pattern.test(teamName) && teamName !== '') {
-        $("#teaName_error_message").hide();
-        $("#teamName").css("border","3px solid #34F458");
-     } else {
-        $("#teamName_error_message").html("Should contain only letters");
-        $("#teamName_error_message").show();
-        $("#teamName").css("border","3px solid #F90A0A");
-        error_teamName = true;
-     }
-  }
-
-  function check_member() {
-    var member = $("#member").val();
-    if ((member <= 4) && (member > 0)) {
-      $("#member_error_message").hide();
-       $("#member").css("border","3px solid #34F458");
-    } else {
-      $("#member_error_message").html("Members Should be from 2 to 4");
-      $("#member_error_message").show();
-      $("#member").css("border","3px solid #F90A0A");
-      error_member = true;
-    }
- }
-
- function check_leaderName() {
-  var pattern = /^[a-zA-Z]*$/;
-  var leaderName = $("#leaderName").val();
-  if (pattern.test(leaderName) &&leaderName !== '') {
-     $("#leaderName_error_message").hide();
-     $("#leaderName").css("border","3px solid #34F458");
-  } else {
-     $("#leaderName_error_message").html("Should contain only letters");
-     $("#leaderName_error_message").show();
-     $("#leaderName").css("border","3px solid #F90A0A");
-     error_leaderName = true;
-  }
-}
-
-function check_leaderNumber() {
-  var leaderNumber_length = $("#leaderNumber").val().length;
-  if ((leaderNumber_length != 10)) {
-     $("#leaderNumber_error_message").html("Number Should have 10 digits");
-     $("#leaderNumber_error_message").show();
-     $("#leaderNumber").css("border","3px solid #F90A0A");
-     error_leaderNumber = true;
-  } else {
-     $("#leaderNumber_error_message").hide();
-     $("#leaderNumber").css("border","3px solid #34F458");
-  }
-}
-
-  function check_leaderEmail() {
-     var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-     var leaderEmail = $("#leaderEmail").val();
-     if (pattern.test(leaderEmail) && leaderEmail !== '') {
-        $("#leaderEmail_error_message").hide();
-        $("#leaderEmail").css("border","3px solid #34F458");
-     } else {
-        $("#leaderEmail_error_message").html("Invalid Email");
-        $("#leaderEmail_error_message").show();
-        $("#leaderEmail").css("border","3px solid #F90A0A");
-        error_leaderEmail = true;
-     }
-  }
-
-  $("#my_form").submit(function() {
-     error_teamName = false;
-     error_member = false;
-     error_leaderName = false;
-     error_leaderNumber = false;
-     error_leaderEmail = false;
-
-     check_teamName();
-     check_member();
-     check_leaderName();
-     check_leaderNumber();
-     check_leaderEmail();
-
-     if (error_teamName === false && error_member === false && error_leaderName === false && error_leaderNumber === false && error_leaderEmail === false) {
-        alert("Registration Successfull");
-        return true;
-     } else {
-        alert("Please Fill the form Correctly");
-        return false;
-     }
+//   $("#teamName").focusout(function(){
+//      check_teamName();
+//   });
+//   $("#member").focusout(function() {
+//      check_member();
+//   });
+//   $("#leaderName").focusout(function() {
+//      check_leaderName();
+//   });
+//   $("#leaderNumber").focusout(function() {
+//      check_leaderNumber();
+//   });
+//   $("#leaderEmail").focusout(function() {
+//      check_leaderEmail();
+//   });
 
 
-  });
-});
+// function check_teamName() {
+//    var pattern = /^[a-zA-Z]*$/;
+//    var teamName_length = $("#teamName").val().length;
+//    var teamName = $("#teamName").val();
+//    if ((pattern.test(teamName)) && (teamName !== '')) {
+//       if(teamName_length < 10){
+//          $("#teamName_error_message").hide();
+//          $("#teamName").css("border","3px solid #34F458");
+//       } else {
+//          $("#teamName_error_message").html("Team Name should be max. 20 characters");
+//          $("#teamName_error_message").show();
+//          $("#teamName").css("border","3px solid #F90A0A");
+//          error_teamName = true;
+//       } 
+//    } else {
+//       $("#teamName_error_message").html("Should contain only letters");
+//       $("#teamName_error_message").show();
+//       $("#teamName").css("border","3px solid #F90A0A");
+//       error_teamName = true;
+//    }
+//  }
+
+//   function check_member() {
+//     var member = $("#member").val();
+//     if ((member <= 4) && (member >=2)) {
+//       $("#member_error_message").hide();
+//        $("#member").css("border","3px solid #34F458");
+//     } else {
+//       $("#member_error_message").html("Members Should be from 2 to 4");
+//       $("#member_error_message").show();
+//       $("#member").css("border","3px solid #F90A0A");
+//       error_member = true;
+//     }
+//  }
+
+//  function check_leaderName() {
+//   var pattern = /^[a-zA-Z]*$/;
+//   var leaderName_length = $("#leaderName").val().length;
+//   var leaderName = $("#leaderName").val();
+//   if ((pattern.test(leaderName)) && (leaderName !== '')) {
+//      if (leaderName_length < 25){
+//       $("#leaderName_error_message").hide();
+//       $("#leaderName").css("border","3px solid #34F458");
+//      } else{
+//       $("#leaderName_error_message").html("Name should be max. 20 characters");
+//       $("#leaderName_error_message").show();
+//       $("#leaderName").css("border","3px solid #F90A0A");
+//      }  
+//   } else {
+//      $("#leaderName_error_message").html("Should contain only letters");
+//      $("#leaderName_error_message").show();
+//      $("#leaderName").css("border","3px solid #F90A0A");
+//      error_leaderName = true;
+//   }
+// }
+
+// function check_leaderNumber() {
+//   var leaderNumber_length = $("#leaderNumber").val().length;
+//   if ((leaderNumber_length != 10)) {
+//      $("#leaderNumber_error_message").html("Number Should have 10 digits");
+//      $("#leaderNumber_error_message").show();
+//      $("#leaderNumber").css("border","3px solid #F90A0A");
+//      error_leaderNumber = true;
+//   } else {
+//      $("#leaderNumber_error_message").hide();
+//      $("#leaderNumber").css("border","3px solid #34F458");
+//   }
+// }
+
+//   function check_leaderEmail() {
+//      var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+//      var leaderEmail = $("#leaderEmail").val();
+//      if (pattern.test(leaderEmail) && leaderEmail !== '') {
+//         $("#leaderEmail_error_message").hide();
+//         $("#leaderEmail").css("border","3px solid #34F458");
+//      } else {
+//         $("#leaderEmail_error_message").html("Invalid Email");
+//         $("#leaderEmail_error_message").show();
+//         $("#leaderEmail").css("border","3px solid #F90A0A");
+//         error_leaderEmail = true;
+//      }
+//   }
+
+//   $("#my_form").submit(function() {
+//      error_teamName = false;
+//      error_member = false;
+//      error_leaderName = false;
+//      error_leaderNumber = false;
+//      error_leaderEmail = false;
+
+//      check_teamName();
+//      check_member();
+//      check_leaderName();
+//      check_leaderNumber();
+//      check_leaderEmail();
+
+//      if (error_teamName === false && error_member === false && error_leaderName === false && error_leaderNumber === false && error_leaderEmail === false) {
+//         alert("Registration Successfull");
+//         return true;
+//      } else {
+//         alert("Please Fill the form Correctly");
+//         return false;
+//      }
+
+
+//   });
+// });
