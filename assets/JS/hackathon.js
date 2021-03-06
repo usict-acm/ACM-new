ce017941ce580190e0bb8e424770e696870f62b4
unknown <anuj.talwar.12@gmail.com>
2021-03-06 22:06:47 +0530

    rocket uda diya

--- assets/JS/hackathon.js
+++ assets/JS/hackathon.js
@@ -31,3 +31,38 @@ $(window).on("scroll", function() {
         }
     );
 });
+
+jQuery(window).scroll(function(){
+    if(jQuery(window).scrollTop()<50){
+        jQuery('#rocketmeluncur').slideUp(500);
+    }else{
+        jQuery('#rocketmeluncur').slideDown(500);
+    }
+    var ftrocketmeluncur = jQuery("#ft")[0] ? jQuery("#ft")[0] : jQuery(document.body)[0];
+    var scrolltoprocketmeluncur = $('rocketmeluncur');
+var viewPortHeightrocketmeluncur = parseInt(document.documentElement.clientHeight);
+var scrollHeightrocketmeluncur = parseInt(document.body.getBoundingClientRect().top);
+var basewrocketmeluncur = parseInt(ftrocketmeluncur.clientWidth);
+var swrocketmeluncur = scrolltoprocketmeluncur.clientWidth;
+if (basewrocketmeluncur < 1000) {
+var leftrocketmeluncur = parseInt(fetchOffset(ftrocketmeluncur)['left']);
+leftrocketmeluncur = leftrocketmeluncur < swrocketmeluncur ? leftrocketmeluncur * 2 - swrocketmeluncur : leftrocketmeluncur;
+scrolltoprocketmeluncur.style.left = ( basewrocketmeluncur + leftrocketmeluncur ) + 'px';
+} else {
+scrolltoprocketmeluncur.style.left = 'auto';
+scrolltoprocketmeluncur.style.right = '10px';
+}
+})
+
+jQuery('#rocketmeluncur').click(function(){
+    jQuery("html, body").animate({ scrollTop: '0px',display:'none'},{
+            duration: 600,  
+            easing: 'linear'
+        });
+    
+    var self = this;
+    this.className += ' '+"launchrocket";
+    setTimeout(function(){
+      self.className = 'showrocket';
+    },800)
+});
