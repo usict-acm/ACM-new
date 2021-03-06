36d05c792f7e0c769da723f9e0d53c933cbe0c36
unknown <anuj.talwar.12@gmail.com>
2021-03-06 22:33:21 +0530

    submssions wala

---
+++ assets/JS/submission-timer.js
@@ -0,0 +1,33 @@
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  let birthday = "Mar 21, 2021 23:59:59",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        // if (distance < 0) {
+        //   let headline = document.getElementById("headline"),
+        //       countdown = document.getElementById("countdown"),
+        //       content = document.getElementById("content");
+        //
+        //   headline.innerText = "Its time for Trellathon!";
+        //   countdown.style.display = "none";
+        //   content.style.display = "block";
+        //
+        //   clearInterval(x);
+        // }
+        //seconds
+      }, 0)
+  }());
