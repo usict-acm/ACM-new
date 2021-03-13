ea09d16b4fa27fe922109c21c0eda0d56fd11cfd
unknown <anuj.talwar.12@gmail.com>
2021-03-14 00:15:17 +0530

    authentication stuff

--- assets/JS/table.js
+++ assets/JS/table.js
@@ -20,48 +20,65 @@ var download = function(content, fileName, mimeType) {
     location.href = 'data:application/octet-stream,' + encodeURIComponent(content); // only this mime type is supported
   }
 }
-db.collection('registration').get().then((s) => {
-  var n = 0;
-  s.forEach(function(x){
-    console.log(x.data());
-    var pusher = [x.data().registration,x.data().teamName, x.data().leaderEmail, x.data().memberOneEmail,  x.data().memberTwoEmail, x.data().memberThreeEmail, x.data().memberFourEmail, x.data().memberOneFirstName, x.data().memberOneLastName,  x.data().memberTwoFirstName, x.data().memberTwoLastName, x.data().memberThreeFirstName, x.data().memberThreeLastName, x.data().memberFourFirstName, x.data().memberFourLastName,  x.data().memberOneGithub, x.data().memberTwoGithub, x.data().memberThreeGithub, x.data().memberFourGithub, x.data().memberOneCollege, x.data().memberTwoCollege, x.data().memberThreeCollege , x.data().memberFourCollege, x.data().memberOneMembership, x.data().memberTwoMembership, x.data().memberThreeMembership, x.data().memberFourMembership, x.data().memberOneNumber, x.data().memberTwoNumber, x.data().memberThreeNumber, x.data().memberFourNumber, x.data().file];
-    data.push(pusher);
-     $("#table").append(`
-<tr class="row">
- <td class="col">
-    `+ x.data().registration +`
-  </td>
-  <td class="col">
-    `+ x.data().teamName +`
-  </td>
-  <td class="col">
-    `+ x.data().leaderEmail +` <br /> `+ x.data().memberOneEmail +` </br> `+ x.data().memberTwoEmail +` </br>  `+ x.data().memberThreeEmail +`<br /> `+ x.data().memberFourEmail +`
-  </td>
-  <td class="col">
-    `+ x.data().memberOneFirstName + ' ' + x.data().memberOneLastName +` <br /> `+ x.data().memberTwoFirstName + ' ' + x.data().memberTwoLastName +` <br /> `+ x.data().memberThreeFirstName + ' ' + x.data().memberThreeLastName +` <br /> `+ x.data().memberFourFirstName + ' ' + x.data().memberFourLastName +`
-  </td>
-  <td class="col">
-     `+ x.data().memberOneGithub +` <br /> `+ x.data().memberTwoGithub +` <br /> `+ x.data().memberThreeGithub +`<br /> `+ x.data().memberFourGithub +`
-  </td>
-  <td class="col">
-     `+ x.data().memberOneCollege +` <br /> `+ x.data().memberTwoCollege +` <br /> `+ x.data().memberThreeCollege +`<br /> `+ x.data().memberFourCollege +`
-  </td>
-  <td class="col">
-     `+ x.data().memberOneMembership +` <br /> `+ x.data().memberTwoMembership +` <br /> `+ x.data().memberThreeMembership +` <br /> `+ x.data().memberFourMembership +`
-  </td>
-  <td class="col">
-     `+ x.data().memberOneNumber +` <br /> `+ x.data().memberTwoNumber +` <br /> `+ x.data().memberThreeNumber +` <br /> `+ x.data().memberFourNumber +`
-  </td>
-  <td class="col">
-    <a target="_blank" href="`+ x.data().file +` ">
-      View Submission
-    </a>
-  </td>
-</tr>
-`);
 
-     n += 1;
+document.getElementById("logger").addEventListener("click", function(event){
+
+firebase.auth().signInWithEmailAndPassword(document.getElementById('email').value, document.getElementById("password").value)
+  .then((userCredential) => {
+    var user = userCredential.user;
+    alert("mauj hogyi");
+    db.collection('registration').get().then((s) => {
+      var n = 0;
+      s.forEach(function(x){
+        console.log(x.data());
+        var pusher = [x.data().registration,x.data().teamName, x.data().leaderEmail, x.data().memberOneEmail,  x.data().memberTwoEmail, x.data().memberThreeEmail, x.data().memberFourEmail, x.data().memberOneFirstName, x.data().memberOneLastName,  x.data().memberTwoFirstName, x.data().memberTwoLastName, x.data().memberThreeFirstName, x.data().memberThreeLastName, x.data().memberFourFirstName, x.data().memberFourLastName,  x.data().memberOneGithub, x.data().memberTwoGithub, x.data().memberThreeGithub, x.data().memberFourGithub, x.data().memberOneCollege.replace(',', ' '), x.data().memberTwoCollege.replace(',', ' '), x.data().memberThreeCollege.replace(',', ' ') , x.data().memberFourCollege.replace(',', ' '), x.data().memberOneMembership, x.data().memberTwoMembership, x.data().memberThreeMembership, x.data().memberFourMembership, x.data().memberOneNumber, x.data().memberTwoNumber, x.data().memberThreeNumber, x.data().memberFourNumber, x.data().file];
+        data.push(pusher);
+        $("#table").append(`
+    <tr class="row">
+    <td class="col">
+        `+ x.data().registration +`
+      </td>
+      <td class="col">
+        `+ x.data().teamName +`
+      </td>
+      <td class="col">
+        `+ x.data().leaderEmail +` <br /> `+ x.data().memberOneEmail +` </br> `+ x.data().memberTwoEmail +` </br>  `+ x.data().memberThreeEmail +`<br /> `+ x.data().memberFourEmail +`
+      </td>
+      <td class="col">
+        `+ x.data().memberOneFirstName + ' ' + x.data().memberOneLastName +` <br /> `+ x.data().memberTwoFirstName + ' ' + x.data().memberTwoLastName +` <br /> `+ x.data().memberThreeFirstName + ' ' + x.data().memberThreeLastName +` <br /> `+ x.data().memberFourFirstName + ' ' + x.data().memberFourLastName +`
+      </td>
+      <td class="col">
+        `+ x.data().memberOneGithub +` <br /> `+ x.data().memberTwoGithub +` <br /> `+ x.data().memberThreeGithub +`<br /> `+ x.data().memberFourGithub +`
+      </td>
+      <td class="col">
+        `+ x.data().memberOneCollege +` <br /> `+ x.data().memberTwoCollege +` <br /> `+ x.data().memberThreeCollege +`<br /> `+ x.data().memberFourCollege +`
+      </td>
+      <td class="col">
+        `+ x.data().memberOneMembership +` <br /> `+ x.data().memberTwoMembership +` <br /> `+ x.data().memberThreeMembership +` <br /> `+ x.data().memberFourMembership +`
+      </td>
+      <td class="col">
+        `+ x.data().memberOneNumber +` <br /> `+ x.data().memberTwoNumber +` <br /> `+ x.data().memberThreeNumber +` <br /> `+ x.data().memberFourNumber +`
+      </td>
+      <td class="col">
+        <a target="_blank" href="`+ x.data().file +` ">
+          View Submission
+        </a>
+      </td>
+    </tr>
+    `);
+
+        n += 1;
+      });
+    });
+
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
   });
+
 });
 
 document.getElementById("csv").addEventListener("click", function(event){
@@ -73,12 +90,9 @@ document.getElementById("csv").addEventListener("click", function(event){
 // Each column is separated by ";" and new line "\n" for next row
 var csvContent = '';
 data.forEach(function(infoArray, index) {
-  dataString = infoArray.join(';');
+  dataString = infoArray.join(',');
   csvContent += index < data.length ? dataString + '\n' : dataString;
 });
-
-// The download function takes a CSV string, the filename and mimeType as parameters
-// Scroll/look down at the bottom of this snippet to see how download is called
 
 download(csvContent, 'dowload.csv', 'text/csv;encoding:utf-8');
 });
