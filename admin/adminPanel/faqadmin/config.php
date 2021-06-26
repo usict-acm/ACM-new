429cb9a82f3bf0a192919e9e5f682ae4308a233f
Anuj Talwar <anuj.talwar.12@gmail.com>
2021-06-26 23:35:09 +0530

    Debugging with bhaiya 1

--- admin/adminPanel/faqadmin/config.php
+++ admin/adminPanel/faqadmin/config.php
@@ -1,19 +1,21 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$DB_SERVER = "localhost";
-$DB_USERNAME = "root";
-$DB_PASSWORD = "";
-$DB_NAME = "acmbackend";
+// $DB_SERVER = "localhost";
+// $DB_USERNAME = "root";
+// $DB_PASSWORD = "";
+// $DB_NAME = "acmbackend";
  
-/* Attempt to connect to MySQL database */
-$link = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+// /* Attempt to connect to MySQL database */
+// $link = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+// // Check connection
+// if($link === false){
+//     die("ERROR: Could not connect. " . mysqli_connect_error());
+// }
 
-$connection = $link;
-
+    require('../blogAdmin/database.php');
+    $database = new Database();
+    $link = $database->connect();
+    $connection = $link;
 ?>
