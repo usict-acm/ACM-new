ab8fd58f94aa3c5d2abb7c618569ca3e22790c20
Kashish Jain <kjcoder04@gmail.com>
2021-07-18 11:51:02 +0530

    fixed login condition on frontend

--- user/dashboard/frontend/src/App.js
+++ user/dashboard/frontend/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   );
 
   return (
-    <BrowserRouter>{!user ? <AdminRoutes /> : <AuthRoutes />}</BrowserRouter>
+    <BrowserRouter>{user ? <AdminRoutes /> : <AuthRoutes />}</BrowserRouter>
   );
 };
 
