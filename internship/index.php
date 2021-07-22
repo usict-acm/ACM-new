667314c6b5e47ff6e0345ccded16b4a548480573
Tarun <tarunshekhawat66@gmail.com>
2021-07-22 21:06:54 +0530

    iframe

--- internship/index.php
+++ internship/index.php
@@ -91,9 +91,39 @@
     </span>
   </div>
   </div>
-<section class="container" style="margin: 10% auto;">
-  <iframe src="./../assets/Summer Internship Proposal.pdf" style="width:100%; height:80vh;"></iframe>
-</section>
+  <div class="container" style="margin: 10% auto">
+      <div
+        style="
+          position: relative;
+          width: 100%;
+          height: 0;
+          padding-top: 56.25%;
+          padding-bottom: 48px;
+          box-shadow: 0 2px 8px 0 rgba(63, 69, 81, 0.16);
+          margin-top: 1.6em;
+          margin-bottom: 0.9em;
+          overflow: hidden;
+          border-radius: 8px;
+          will-change: transform;
+        "
+      >
+        <iframe
+          loading="lazy"
+          style="
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            border: none;
+            padding: 0;
+            margin: 0;
+          "
+          src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEiOfwdDVQ&#x2F;view?embed"
+        >
+        </iframe>
+      </div>
+    </div>
  
 <!-- =====================================Contact us============================ -->
 
