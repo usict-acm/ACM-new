5a660d3ba338875a585287fc2f763be287ee2a8a
Ajit-91 <ajitkumar12105@gmail.com>
2021-08-05 21:01:54 +0530

    updated profile functionality

--- user/dashboard/frontend/src/pages/Admin/Profile.js
+++ user/dashboard/frontend/src/pages/Admin/Profile.js
@@ -1,4 +1,4 @@
-import React,{ useState } from "react";
+import React,{ useEffect, useState } from "react";
 import {
   CardImg,
   Button,
@@ -17,11 +17,58 @@ import { useSelector } from "react-redux";
 import { selectUser } from "redux/slices/userSlice";
 
 const Profile = (props) => {
+  const user = useSelector(selectUser);
+
   const[disabled,setDisabled] = useState(true);
   const[cursor, setCursor] = useState({cursor : ""});
-//   const[readOnly,setReadonly] = useState(true);
-  const user = useSelector(selectUser);
-  console.log(user);
+  const [data, setData] = useState([]);
+
+  const[name, setName] = useState();
+  const[memberId, setMemberId] = useState();
+  const[id, setId] = useState();
+  const[email, setEmail] = useState()
+  const[branch, setBranch] = useState()
+  const[rollNo, setRollNo] = useState()
+  const[course, setcourse] = useState()
+
+
+const url = "http://localhost:8000/users";
+
+const getUser = () =>{
+  fetch(url).then((result) =>{
+    result.json().then((resp) =>{
+        setData(resp?.[0]);
+        setName(resp?.[0]?.name);
+        setMemberId(resp?.[0]?.memberId);
+        setId(resp?.[0]?.id);
+        setEmail(resp?.[0]?.email);
+        setRollNo(resp?.[0]?.rollNo);
+        setBranch(resp?.[0]?.branch);
+        setcourse(resp?.[0]?.course);
+    })
+  })
+};
+
+useEffect( ()=>{
+  getUser();
+}, [ ] );
+
+  const updateInfo = () =>{
+    let items = {id, name,email, memberId,course, rollNo,branch }
+    fetch(`http://localhost:8000/users/${id}`, {
+      method : 'PUT',
+      headers : {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify(items)
+    }).then((result)=>{
+      result.json().then((resp)=>{
+        // getUser();
+      })
+    })
+  }
+
   return (
     <Container className="mt-0" fluid="xxl">
       <UserHeader />
@@ -59,9 +106,13 @@ const Profile = (props) => {
                 />
                 <div className="my-3">
                  {disabled ?
-                  <h2 className="text-center my-3">{user?.name}</h2> 
+                  <h2 className="text-center my-3">
+                  {/* {user?.name} */}
+                  {name}
+                  </h2> 
                   : <Input className="text-center m-auto d-block" type="name"
-                     placeholder={user?.name} readOnly={disabled} style={{ width: "25%" }}                           
+                     placeholder={user?.name} readOnly={disabled} style={{ width: "25%" }}   
+                     onChange={(e)=>setName(e.target.value)}   value={name}                     
                 />
                   }
                 </div>
@@ -78,7 +129,8 @@ const Profile = (props) => {
                             type="email"
                             placeholder={user?.email} 
                             readOnly    
-                            style={cursor}                       
+                            value={data?.email}
+                            style={cursor}                    
                           />
                         </FormGroup>
                       </Col>
@@ -89,6 +141,7 @@ const Profile = (props) => {
                             type="text"
                             placeholder={user?.course}
                             readOnly
+                            value={data?.course}
                             style={cursor}
                           />
                         </FormGroup>
@@ -100,6 +153,7 @@ const Profile = (props) => {
                             type="text"
                             placeholder={user?.branch}
                             readOnly
+                            value={data?.branch}
                             style={cursor}
                           />
                         </FormGroup>
@@ -113,6 +167,7 @@ const Profile = (props) => {
                             type="text"
                             placeholder={user?.rollNo}
                             readOnly
+                            value={data?.rollNo}
                             style={cursor}
                           />
                         </FormGroup>
@@ -126,6 +181,8 @@ const Profile = (props) => {
                             type="text"
                             placeholder={user?.acmMemberId}
                             readOnly = {disabled}
+                            onChange={(e)=>setMemberId(e.target.value)}
+                            value={memberId}
                           />
                         </FormGroup>
                       </Col>
@@ -134,12 +191,16 @@ const Profile = (props) => {
                        {disabled ? null : <Button onClick={()=>{
                            setDisabled(true);
                            setCursor({cursor : ""});
+                           updateInfo();
                            }}
                         type="button" value="Input" color="success" >
                         Save changes
                        </Button>}
-                       {disabled ? null : <Button onClick={()=>setDisabled(true)}
-                        type="button" value="Input" color="primary" >
+                       {disabled ? null : <Button onClick={()=>{
+                           setDisabled(true)
+                           window.location.reload();
+                         }}
+                        type="button" value="Input" color="danger" >
                         Cancel
                        </Button>}
                       </div>
