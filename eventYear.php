1e6145af63846ca94b7e44d47dbb2346db7d8ba7
Harsh Goyal <harsh.student99@gmail.com>
2021-08-24 21:29:55 +0530

    Speakers and partners fixes\

--- eventYear.php
+++ eventYear.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php
-        include('./head.php');
+    include('./head.php');
     ?>
     <title>USICT ACM</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" />
@@ -11,8 +11,7 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Poppins:wght@200&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Poppins:wght@200&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 
     <link rel="stylesheet" type="text/css" href="./assets/CSS/glider.css" />
@@ -20,51 +19,53 @@
 </head>
 
 <body>
-<?php
-      // $currentPage = 'blogs';
-      include('./header.php');
-  ?>
-    
+    <?php
+    // $currentPage = 'blogs';
+    include('./header.php');
+    ?>
+
     <!-- ==============================================Content========================================== -->
 
-<section id="content">
-    <div class="event-container">
-        <div id="eventPage"></div>
-    </div>
-    <div class="row pagination_row">
-        <div style="margin:auto;" id="pagination_row_2021"></div>
-    </div>
-</section>
+    <section id="content">
+        <div class="event-container">
+            <div id="eventPage"></div>
+        </div>
+        <div class="row pagination_row">
+            <div style="margin:auto;" id="pagination_row_2021"></div>
+        </div>
+    </section>
 
-<?php
+    <?php
     $page = isset($_GET["page"]) ? $_GET["page"] : 1;
     $year = $_GET["year"];
     $previous = $page == 1 ? 1 : $page - 1;
-?>
+    ?>
     <script>
-        let allEvents=document.getElementById("eventPage");
-        if(<?php echo $year ?> != 2019 && <?php echo $year ?> != 2020 && <?php echo $year ?> != 2021){
+        let allEvents = document.getElementById("eventPage");
+        if (<?php echo $year ?> != 2019 && <?php echo $year ?> != 2020 && <?php echo $year ?> != 2021) {
             allEvents.innerHTML += `<h1 class='noEvents'>No Events for this year</h1>`;
         } else {
-        let urlEvent = './admin/blogAdmin/api.php/?q=readAllEvent&year='+<?php echo $year ?>+'&page='+<?php echo $page ?>;
+            let urlEvent = './admin/blogAdmin/api.php/?q=readAllEvent&year=' + <?php echo $year ?> + '&page=' + <?php echo $page ?>;
 
-        let pagination = document.getElementById("pagination_row_2021");
-        allEvents.innerHTML="";
-        
-        $(document).ready(function() {
-            $.ajax({
-                url: urlEvent,
-                method: 'GET',
-                dataType: 'JSON',
-                success: function(data) {
-                    console.log("check data ywar",data);
-                    data[0].forEach(allEvent);                    
-                    function allEvent(event) {
+            let pagination = document.getElementById("pagination_row_2021");
+            allEvents.innerHTML = "";
 
-                        let button1=  ` <a href=${event.button1Link} style="float: right;" target="_blank"><button class="dateTime-para btn form-input-boxes-manual btn-watch" style="font-size: 16px;">${event.button1Text}</button></a>`;
-                        let button2=`<a href=${event.button2Link} style="float: right;" target="_blank"><button class="dateTime-para btn form-input-boxes-manual event-button-two btn-watch" style="font-size: 16px;">${event.button2Text}</button></a>`;
-                     
-                        let var1 = `<div class="event-post">
+            $(document).ready(function() {
+                $.ajax({
+                    url: urlEvent,
+                    method: 'GET',
+                    dataType: 'JSON',
+                    success: function(data) {
+                        console.log("check data ywar", data);
+                        data[0].forEach(allEvent);
+
+                        function allEvent(event) {
+
+                            let button1 = ` <a href=${event.button1Link} style="float: right;" target="_blank"><button class="dateTime-para btn form-input-boxes-manual btn-watch" style="font-size: 16px;">${event.button1Text}</button></a>`;
+                            let button2 = `<a href=${event.button2Link} style="float: right;" target="_blank"><button class="dateTime-para btn form-input-boxes-manual event-button-two btn-watch" style="font-size: 16px;">${event.button2Text}</button></a>`;
+                            let eventPartner = `<p class="dateTime-para">Partnered By: ${event.partners}</p>`
+                            let eventSpeaker = `<p class="dateTime-para">Speakers: ${event.speakers}</p>`
+                            let var1 = `<div class="event-post">
                                             <div class="event-post_img">
                                                 <img src=${event.poster}  alt="">
                                             </div>
@@ -75,130 +76,133 @@
                                                 </div>
                                                 <h1 class="event-post_title">${event.name}</h1>
                                                 <p class="event-post_text">${event.description}</p>
-                                                <p class="dateTime-para">${event.partners}</p>
-                                                <p class="dateTime-para">${event.speakers}</p>
                                                 <div class="buttonDiv">
                                     `;
-                        let closeDIV=`           </div>
+                            let closeDIV = `           </div>
                                             </div>
                                         </div>`;
-                        if(event.button2Text!==''&&event.button1Text!==''){
-                            let finalCode=var1.concat(button1,button2,closeDIV);
-                            $("#eventPage").append(finalCode);
+
+                            if (event.partners !== '') {
+                                var1 = var1 + eventPartner;
+                            }
+                            if (event.speakers !== '') {
+                                var1 = var1 + eventSpeaker
+                            }
+
+
+                            if (event.button2Text !== '' && event.button1Text !== '') {
+                                let finalCode = var1.concat(button1, button2, closeDIV);
+                                $("#eventPage").append(finalCode);
+                            } else if (event.button1Text !== '' && event.button2Text === '') {
+                                let finalCode = var1.concat(button1, closeDIV);
+                                $("#eventPage").append(finalCode);
+                            } else if (event.button2Text !== '' && event.button1Text === '') {
+                                let finalCode = var1.concat(button1, closeDIV);
+                                $("#eventPage").append(finalCode);
+                            } else {
+                                let finalCode = var1.concat(closeDIV);
+                                $("#eventPage").append(finalCode);
+                            }
+
                         }
-                        else if(event.button1Text!==''&&event.button2Text===''){
-                            let finalCode=var1.concat(button1,closeDIV);
-                            $("#eventPage").append(finalCode);
-                        }
-                        else if(event.button2Text!==''&&event.button1Text===''){
-                            let finalCode=var1.concat(button1,closeDIV);
-                            $("#eventPage").append(finalCode);
-                        }
-                        else{
-                            let finalCode=var1.concat(closeDIV);
-                            $("#eventPage").append(finalCode);
-                        }
-                                                          
-                    } 
-		    pagination.innerHTML += "<nav style=display:inline-block; aria-label=Page navigation example>\
+                        pagination.innerHTML += "<nav style=display:inline-block; aria-label=Page navigation example>\
                                                <ul class=pagination>\
                                                   <li class=page-item >\
-                                                    <a id='previous-pagination' class='page-link pagination-option-next-prev' href=./eventYear.php?q=readAllEvent&year="+<?php echo $year ?>+"&page=<?php echo $previous ?> disabled>\
+                                                    <a id='previous-pagination' class='page-link pagination-option-next-prev' href=./eventYear.php?q=readAllEvent&year=" + <?php echo $year ?> + "&page=<?php echo $previous ?> disabled>\
                                                       < Prev\
                                                     </a>\
                                                   </li>\
                                                </ul>\
                                              </nav>";
-                   if (<?php echo $page ?> === 1) {
-                      document.getElementById("previous-pagination").className += " disabled-pagination";
-                   }
- 		   for (var i = 1; i <= data[2]; i++) {
-                      var id = "link_pagination" + i;
-                      pagination.innerHTML += "<nav style=display:inline-block; aria-label=Page navigation example>\
+                        if (<?php echo $page ?> === 1) {
+                            document.getElementById("previous-pagination").className += " disabled-pagination";
+                        }
+                        for (var i = 1; i <= data[2]; i++) {
+                            var id = "link_pagination" + i;
+                            pagination.innerHTML += "<nav style=display:inline-block; aria-label=Page navigation example>\
                                                   <ul class=pagination>\
                                                      <li class=page-item>\
-                                                        <a id=" + id + " class='page-link pagination-numbers pagination-option-next-prev '" + "href=./eventYear.php?q=readAllEvent&year="+<?php echo $year ?>+"&page=" + i + ">\
+                                                        <a id=" + id + " class='page-link pagination-numbers pagination-option-next-prev '" + "href=./eventYear.php?q=readAllEvent&year=" + <?php echo $year ?> + "&page=" + i + ">\
                                                            " + i + "\
                                                         </a>\
                                                      </li>\
                                                   </ul>\
                                                </nav></div>"
-                     if (<?php echo $page ?> === i) {
-                        console.log(<?php echo $page ?>);
-                        document.getElementById("link_pagination" + i).className += "active_pagination";
-                     }
-                  }
-		  var next = <?php echo $page ?> === data[2] ? data[2] : <?php echo $page + 1 ?>;
-                  pagination.innerHTML += "<nav style=display:inline-block; aria-label=Page navigation example>\
+                            if (<?php echo $page ?> === i) {
+                                console.log(<?php echo $page ?>);
+                                document.getElementById("link_pagination" + i).className += "active_pagination";
+                            }
+                        }
+                        var next = <?php echo $page ?> === data[2] ? data[2] : <?php echo $page + 1 ?>;
+                        pagination.innerHTML += "<nav style=display:inline-block; aria-label=Page navigation example>\
                                               <ul class=pagination>\
                                                  <li class=page-item>\
-                                                    <a id='next-pagination' class='page-link pagination-option-next-prev' href=./eventYear.php?q=readAllEvent&year="+<?php echo $year ?>+"&page=" + next + ">\
+                                                    <a id='next-pagination' class='page-link pagination-option-next-prev' href=./eventYear.php?q=readAllEvent&year=" + <?php echo $year ?> + "&page=" + next + ">\
                                                         Next >\
                                                     </a>\
                                                  </li>\
                                               </ul>\
                                           </nav>";
-		 if (<?php echo $page ?> === data[2]) {
-                    document.getElementById("next-pagination").className += " disabled-pagination";
-                }
-              },
+                        if (<?php echo $page ?> === data[2]) {
+                            document.getElementById("next-pagination").className += " disabled-pagination";
+                        }
+                    },
+                });
             });
-        });
-    }
-</script>  
+        }
+    </script>
 
-<style>
-    .pagination_row{
-        margin-right: 0;
-        margin-left:0;
-    }
+    <style>
+        .pagination_row {
+            margin-right: 0;
+            margin-left: 0;
+        }
 
-    .pagination-option-next-prev {
-      border: 0.5px solid gray;
-      color: black;
-      box-shadow: rgb(0 0 0 / 17%) 2px 2px 10px;
-      font-size: 23px;
-      margin: 40px 10px 20px 10px !important;
-    }
+        .pagination-option-next-prev {
+            border: 0.5px solid gray;
+            color: black;
+            box-shadow: rgb(0 0 0 / 17%) 2px 2px 10px;
+            font-size: 23px;
+            margin: 40px 10px 20px 10px !important;
+        }
 
-    .pagination-numbers {
-      border: 2px solid #0297ff;
-      box-shadow: none;
-      padding-left: 15px !important;
-      padding-right: 15px !important;
-      border-radius: 50% !important;
-    }
+        .pagination-numbers {
+            border: 2px solid #0297ff;
+            box-shadow: none;
+            padding-left: 15px !important;
+            padding-right: 15px !important;
+            border-radius: 50% !important;
+        }
 
-    .active_pagination {
-      border: 2px solid #0297ff;
-      background-color: #0297ff;
-      color: white;
-      padding-left: 15px !important;
-      padding-right: 15px !important;
-      border-radius: 50% !important;
-    }
+        .active_pagination {
+            border: 2px solid #0297ff;
+            background-color: #0297ff;
+            color: white;
+            padding-left: 15px !important;
+            padding-right: 15px !important;
+            border-radius: 50% !important;
+        }
 
-    .disabled-pagination {
-      color: lightgray;
-      pointer-events: none;
-      cursor: default;
-      border: none;
-      box-shadow: none;
-      font-size: 23px;
-      margin: 40px 10px 20px 10px !important;
-    }
+        .disabled-pagination {
+            color: lightgray;
+            pointer-events: none;
+            cursor: default;
+            border: none;
+            box-shadow: none;
+            font-size: 23px;
+            margin: 40px 10px 20px 10px !important;
+        }
 
-    .noEvents{
-        font-size: 4rem;
-        padding: 80px 20px;
-        text-align: center;
-    }
+        .noEvents {
+            font-size: 4rem;
+            padding: 80px 20px;
+            text-align: center;
+        }
+    </style>
 
-</style>
-
- <!-- ==============================================Footer========================================== -->
- <?php
-      include('footer.php');
+    <!-- ==============================================Footer========================================== -->
+    <?php
+    include('footer.php');
     ?>
 </body>
 
--- events.php
+++ events.php
@@ -3,14 +3,13 @@
 
 <head>
   <?php
-    include('./head.php');
+  include('./head.php');
   ?>
   <title>USICT ACM</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="./assets/CSS/events.css" rel="stylesheet" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Poppins:wght@200&display=swap"
-    rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Poppins:wght@200&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
   <script src="https://kit.fontawesome.com/d459eda8d9.js" crossorigin="anonymous"></script>
 </head>
@@ -18,8 +17,8 @@
 <body>
   <!-- ************************************************Navbar******************************************************************************-->
   <?php
-      // $currentPage = 'blogs';
-      include('./header.php');
+  // $currentPage = 'blogs';
+  include('./header.php');
   ?>
 
   <section>
@@ -29,9 +28,9 @@
         <p>“Words can do only much without actions”</p>
         <p>We organize various events and workshops every year to make sure that you learn with the competition. Let’s go through the highlights of this enthralling journey that promises to get better with time.</p>
       </div>
-      
-                      <div class="content live-event">
-                        <!-- <span class="badge">
+
+      <div class="content live-event">
+        <!-- <span class="badge">
                           <span class="badge-wrap">
                             <span class="badge-icon"><svg height="14" viewBox="0 0 14 14" width="14" xmlns="http://www.w3.org/2000/svg">
                                 <path
@@ -44,11 +43,11 @@
                                 </svg></span></span><span class="badge-text">LIVE</span>
                           </span>
                         </span> -->
-                        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" data-interval=3000>
-                            
-                          <div class="carousel-inner" id="carousel" style="height: 100%;"></div>
-                            
-                          <!-- <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" data-interval=3000>
+
+          <div class="carousel-inner" id="carousel" style="height: 100%;"></div>
+
+          <!-- <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
                           </a>
@@ -57,47 +56,48 @@
                             <span class="sr-only">Next</span>
                           </a> -->
 
-                        </div>
-                      </div>
+        </div>
+      </div>
 
 
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-    let urlcarousel = './admin/blogAdmin/api.php/?q=carousel';
-    let carouselDIV=document.getElementById("carousel");
+    <script>
+      let urlcarousel = './admin/blogAdmin/api.php/?q=carousel';
+      let carouselDIV = document.getElementById("carousel");
 
-    $(document).ready(function() {
-      $.ajax({
-        url: urlcarousel,
-        method: 'GET',
-        dataType: 'JSON',
-            success: function(data) {
-                console.log("check data 123 ",data);
-                carouselDIV.innerHTML +=" <div class='carousel-item active'>\
-                                                    <img src="+data[0].poster+"  class='live-carousel-img'>\
-                                                  </div>"; 
-                console.log(data[0].poster);
-                console.log(carouselDIV);
-                function carouselfunction() {
+      $(document).ready(function() {
+        $.ajax({
+          url: urlcarousel,
+          method: 'GET',
+          dataType: 'JSON',
+          success: function(data) {
+            console.log("check data 123 ", data);
+            carouselDIV.innerHTML += " <div class='carousel-item active'>\
+                                                    <img src=" + data[0].poster + "  class='live-carousel-img'>\
+                                                  </div>";
+            console.log(data[0].poster);
+            console.log(carouselDIV);
 
-                        for (i=1;i<data.length;i++){
-                          carouselDIV.innerHTML +=" <div class='carousel-item'>\
-                                                      <img src="+data[i].poster+"  class='live-carousel-img'>\
-                                                    </div>";                             
-                        }
-                    }
-                    carouselfunction();
-                console.log(carouselDIV);
-            },
-          });
-    });
-</script> 
+            function carouselfunction() {
 
-    
+              for (i = 1; i < data.length; i++) {
+                carouselDIV.innerHTML += " <div class='carousel-item'>\
+                                                      <img src=" + data[i].poster + "  class='live-carousel-img'>\
+                                                    </div>";
+              }
+            }
+            carouselfunction();
+            console.log(carouselDIV);
+          },
+        });
+      });
+    </script>
+
+
     <div class="events">
       <ul id="eventIndex">
-        
+
       </ul>
     </div>
   </section>
@@ -109,22 +109,22 @@
     let url = './admin/blogAdmin/api.php/?q=getImage';
     var galleryImage = document.getElementById("galleryImage");
     galleryImage.innerHTML = "";
-    
+
     $(document).ready(function() {
       $.ajax({
         url: url,
         method: 'GET',
         dataType: 'JSON',
         success: function(data) {
-          console.log("gallery data",data);
+          console.log("gallery data", data);
           data.forEach(myFunc);
 
           function myFunc(row, index) {
-            galleryImage.innerHTML+=" <img class='quote-img' src="+ row.Image+ " alt=' ' />";
-              }
-            }
-          })
-        })  
+            galleryImage.innerHTML += " <img class='quote-img' src=" + row.Image + " alt=' ' />";
+          }
+        }
+      })
+    })
   </script>
 
   <!-- <div class="quotes">
@@ -134,12 +134,12 @@
     <img class="quote-img" src="./assets/images/About us/image4.jpg" alt="" />
     <img class="quote-img" src="./assets/images/trell-4.jpeg" alt="" />
   </div> -->
-   
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  
-<script>
+
+  <script>
     let urlEventIndex = './admin/blogAdmin/api.php/?q=getYearEvent';
-    let yearWiseEvent=document.getElementById("eventIndex");
+    let yearWiseEvent = document.getElementById("eventIndex");
     let numberOfEvents = 0;
 
     $(document).ready(function() {
@@ -148,10 +148,11 @@
         method: 'GET',
         dataType: 'JSON',
         success: function(data) {
-          console.log("check data ",data);
+          console.log("check data ", data);
           data.forEach(allEventmore);
+
           function allEventmore(event) {
-            let urlYear = './admin/blogAdmin/api.php/?q=readAllEvent&year='+event.year;
+            let urlYear = './admin/blogAdmin/api.php/?q=readAllEvent&year=' + event.year;
             $.ajax({
               url: urlYear,
               method: 'GET',
@@ -159,36 +160,36 @@
               dataType: 'JSON',
               success: function(dataNumber) {
                 console.log(urlYear);
-                console.log("dataNumber",dataNumber);
+                console.log("dataNumber", dataNumber);
                 numberOfEvents = dataNumber[1];
-                console.log("numberrrr",numberOfEvents);
-                yearWiseEvent.innerHTML +=" <li>\
+                console.log("numberrrr", numberOfEvents);
+                yearWiseEvent.innerHTML += " <li>\
                                                 <div class='time'>\
-                                                    <h2>"+event.year+" <br><span></span></h2>\
+                                                    <h2>" + event.year + " <br><span></span></h2>\
                                                 </div>\
                                                 <div class='details'>\
-                                                    <h3 style='color: black;font-weight: bolder;'>"+numberOfEvents+" "+event.numberOfEvents+" </h3>\
-                                                    <p style='font-size:large; color: #e91e63;font-weight: bolder;'>"+event.heading+"</p>\
-                                                    <p style='text-align: end; margin-right: 2px; font-size: smaller; color:#0297ff;'>"+event.more+"</p>\
-                                                    <a  href='./eventYear.php?year="+event.year+"'>View details</a>\
+                                                    <h3 style='color: black;font-weight: bolder;'>" + numberOfEvents + " " + event.numberOfEvents + " </h3>\
+                                                    <p style='font-size:large; color: #e91e63;font-weight: bolder;'>" + event.heading + "</p>\
+                                                    <p style='text-align: end; margin-right: 2px; font-size: smaller; color:#0297ff;'>" + event.more + "</p>\
+                                                    <a  href='./eventYear.php?year=" + event.year + "'>View details</a>\
                                                 </div>\
                                                 <div style='clear: both;'></div>\
                                             </li>";
               }
-            })                              
+            })
           }
         },
-      error:function(error){
-        console.log(error);
-      },
+        error: function(error) {
+          console.log(error);
+        },
       });
     });
-</script>
-  
-    <!-- ***********************************************Footer************************************************************************ -->
-    <?php
-      include('footer.php');
-    ?>
+  </script>
+
+  <!-- ***********************************************Footer************************************************************************ -->
+  <?php
+  include('footer.php');
+  ?>
 
 </body>
 
