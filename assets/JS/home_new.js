d486432cb24588ef711f1c86e19756ff870cec14
yogesh <neemaker246@gmail.com>
2021-09-07 17:05:58 +0530

    contact us section

---
+++ assets/JS/home_new.js
@@ -0,0 +1,10 @@
+// *************************contact_us function**********************************************
+function showcontact(){
+    document.getElementById("contact").style.display= "block";
+    document.getElementById("contactus").style.display= "none"
+}
+function closecontact(){
+    document.getElementById("contact").style.display= "none";
+    document.getElementById("contactus").style.display= "block"
+}
+// ****************************contact us function end*******************************************
