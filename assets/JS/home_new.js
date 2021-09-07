86d613b755384d957a4ca1f7d8711202b654a1e0
Mayank Bisht <bishtmayank799@gmail.com>
2021-09-07 21:16:35 +0530

    added hero and about us section

--- assets/JS/home_new.js
+++ assets/JS/home_new.js
@@ -1,10 +1,76 @@
 // *************************contact_us function**********************************************
-function showcontact(){
-    document.getElementById("contact").style.display= "block";
-    document.getElementById("contactus").style.display= "none"
+function showcontact() {
+	document.getElementById('contact').style.display = 'block';
+	document.getElementById('contactus').style.display = 'none';
 }
-function closecontact(){
-    document.getElementById("contact").style.display= "none";
-    document.getElementById("contactus").style.display= "block"
+function closecontact() {
+	document.getElementById('contact').style.display = 'none';
+	document.getElementById('contactus').style.display = 'block';
 }
 // ****************************contact us function end*******************************************
+
+// ******************hero section ************************************************
+// HERO SLIDER
+var menu = [];
+jQuery('.swiper-slide').each(function (index) {
+	menu.push(jQuery(this).find('.slide-inner').attr('data-text'));
+});
+var interleaveOffset = 0.5;
+var swiperOptions = {
+	loop: true,
+	speed: 1000,
+	parallax: true,
+	autoplay: {
+		delay: 6500,
+		disableOnInteraction: false,
+	},
+	watchSlidesProgress: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	on: {
+		progress: function () {
+			var swiper = this;
+			for (var i = 0; i < swiper.slides.length; i++) {
+				var slideProgress = swiper.slides[i].progress;
+				var innerOffset = swiper.width * interleaveOffset;
+				var innerTranslate = slideProgress * innerOffset;
+				swiper.slides[i].querySelector('.slide-inner').style.transform =
+					'translate3d(' + innerTranslate + 'px, 0, 0)';
+			}
+		},
+
+		touchStart: function () {
+			var swiper = this;
+			for (var i = 0; i < swiper.slides.length; i++) {
+				swiper.slides[i].style.transition = '';
+			}
+		},
+
+		setTransition: function (speed) {
+			var swiper = this;
+			for (var i = 0; i < swiper.slides.length; i++) {
+				swiper.slides[i].style.transition = speed + 'ms';
+				swiper.slides[i].querySelector('.slide-inner').style.transition = speed + 'ms';
+			}
+		},
+	},
+};
+
+var swiper = new Swiper('.swiper-container', swiperOptions);
+
+// DATA BACKGROUND IMAGE
+var sliderBgSetting = $('.slide-bg-image');
+sliderBgSetting.each(function (indx) {
+	if ($(this).attr('data-background')) {
+		$(this).css('background-image', 'url(' + $(this).data('background') + ')');
+	}
+});
+// ************************hero section ends*********************************
