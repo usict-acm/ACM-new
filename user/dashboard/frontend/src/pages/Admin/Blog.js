83f43de6e618dbaf4a9e40acaba3b3dbf37f2601
Ajit-91 <ajitkumar12105@gmail.com>
2021-09-10 20:40:02 +0530

    added media quiries in blog

--- user/dashboard/frontend/src/pages/Admin/Blog.js
+++ user/dashboard/frontend/src/pages/Admin/Blog.js
@@ -14,40 +14,49 @@ import { useSelector } from 'react-redux';
 import { selectBlogs } from 'redux/slices/blogSlice';
 
 
-function Content(props){
-  return(
-    <div className="contentBox">
-        <div className="smallBox">
 
-        
-            <input type="checkbox"/>
-            <div className="details">
-              <h3>{props.title}</h3>
-              <h5>{props.date}</h5>
-            </div>
-       </div>
-       <div>
-         <Button>Edit</Button>
-         <Button>Trash</Button>
-       </div>
-    </div>
-  )
-}
 
 export default function Blog() {
   const blogs = useSelector(selectBlogs)
   console.log(blogs)
 
   // const [data,setData] = useState(false)
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const [selected, setSelected] = useState(false);
+  console.log( "selected : "+ selected);
   const toggle = () => setDropdownOpen(prevState => !prevState);
 
-  // function showData() {
-  //   if(blogs[0].blogId === false){
-  //     return false
-  //   }
-  //   else return true
-  // }
+  function showData() {
+    if(blogs[0].blogId === false){
+      return false
+    }
+    else return true
+  }
+
+  function Content(props){
+    return(
+      <div className="contentBox" >
+  
+  <div className="smallBox">
+          
+              <input type="checkbox"
+              //  onClick={()=>setDisabled(!disabled)}
+                 onChange = {(e)=> setSelected(e.target.checked)}
+                 
+               />
+
+              <div className="details">
+                <h3>{props.title}</h3>
+                <p>{props.date}</p>
+              </div>
+         </div>
+         <div className="Buttons">
+           <Button color="info">Edit</Button>
+           <Button color="danger">Trash</Button>
+         </div>
+      </div>
+    )
+  }
   
     return (
         <div>
@@ -74,7 +83,7 @@ export default function Blog() {
                    <Col className="text-right post-btn">
                      <Button
                       type="button"
-                      color="info"
+                      color="success"
                       href="/CreateBlog"
                      >
                      New Post
@@ -89,31 +98,46 @@ export default function Blog() {
                        <Card className="bg-secondary">
                         <CardHeader className="bg-white border-0">
                          <Row>
-                          <Col className="text-right" sm={{ size: 4, offset: 8 }}>
-                           <Input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                           <Col lg="8" md="6" sm="4" xs="4">
+                            <div>
+                            {
+                             selected ? 
+                           
+                            <Button type="button" color="danger">Trash All</Button>
+                            : null
+                            }
+                            </div>
+                           </Col>
+                          <Col lg="4" md="6" sm="8" xs="8">
+                           <Input 
+                            // className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"/>
                           </Col>
                          </Row>
                         </CardHeader>
                         <hr className="h-line"/>
                         <CardBody className="post-section">
 
-                          {/* <div>                     
-                            { showData() === true ?  <h3>NO DATA YET</h3> : 
+                          <div>                     
+                            { showData() !== true ?
+                            <div className="cont-nodata"><h3>NO DATA YET</h3></div>
+                             : 
                             <div className="post-component">
-                                {blogs.map((item)=>(
-                                <Content id="post-content" title={item.blogTitle} date={item.published} key={item.blogId}/>
-                                ))
+                                {blogs.map( (item, i) => (
+                                <Content id="post-content" 
+                                title={item.blogTitle} date={item.published} key={i} blogId = {item.blogId} />))
                                 }
-                            </div>
-                            }
-                          </div> */}
+                            </div>}
+                          </div>
+
                         </CardBody>
                        </Card>
                       </Col>
                     </Row>
                   </Container>
                  </Container>
-
                 </Card>
               </Col>
             </Row>
@@ -121,5 +145,5 @@ export default function Blog() {
         </Container>
       </>
     </div>
-  )
+  );
 }
