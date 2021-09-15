075946e3f7462dac2d257b3cf97593f2b9ea5acc
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-09-15 16:24:26 +0530

    delete api

--- user/dashboard/backend/blogApi.php
+++ user/dashboard/backend/blogApi.php
@@ -92,14 +92,14 @@ function fetchUserBlogs()
 function updateBlog()
 {
     if ($_SERVER['REQUEST_METHOD'] !== "POST") {
-        echo "Cannot " . $_SERVER['REQUEST_METHOD'] . " /createBlog";
+        echo "Cannot " . $_SERVER['REQUEST_METHOD'] . " /updateBlog";
         return;
     }
 
     $blog = blogInit();
     $req = json_decode(file_get_contents('php://input'), true);
     if ($req["blogId"] && $req["blogTitle"] && $req["userEmail"] && $req["content"]) {
-        $result = $blog->updateBlog($req);        
+        $result = $blog->updateBlog($req);
         if ($result) {
             $allBlogs = $blog->fetchAllBlogs($req["userEmail"]);
             $allBlogsArr = array();
@@ -126,6 +126,47 @@ function updateBlog()
         }
     } else {
         echo json_encode(array('error' => 'One or more fields are missing.'));
+        return;
+    }
+}
+
+function deleteBlog()
+{
+    if ($_SERVER['REQUEST_METHOD'] !== "POST") {
+        echo "Cannot " . $_SERVER['REQUEST_METHOD'] . " /deleteBlog";
+        return;
+    }
+
+    $blog = blogInit();
+    $req = json_decode(file_get_contents('php://input'), true);
+    if ($req["blogId"] && $req["userEmail"]) {
+        $result = $blog->deleteBlog($req["blogId"]);
+        if ($result) {
+            $allBlogs = $blog->fetchAllBlogs($req["userEmail"]);
+            $allBlogsArr = array();
+            if ($allBlogs->num_rows > 0) {
+                while ($row = $allBlogs->fetch_assoc()) {
+                    $blogRow = array(
+                        "blogId" => $row["blogId"],
+                        "blogTitle" => $row["blogTitle"],
+                        "content" => $row["content"],
+                        "userEmail" => $row["userEmail"],
+                        "userName" => $row["userName"],
+                        "isDraft" => boolval($row["isDraft"]),
+                        "tags" => unserialize($row["tags"]),
+                        "created" => $row["created"],
+                        "published" => $row["published"],
+                        "lastUpdated" => $row["lastUpdated"]
+                    );
+                    array_push($allBlogsArr, $blogRow);
+                }
+                echo json_encode(array("message" => "success", "blogs" => $allBlogsArr));
+            } else {
+                echo json_encode(array("error" => "No Blogs"));
+            };
+        }
+    } else {
+        echo json_encode(array('error' => 'Missing id'));
         return;
     }
 }
--- user/dashboard/backend/blogFunctions.php
+++ user/dashboard/backend/blogFunctions.php
@@ -1,19 +1,22 @@
 <?php
 
-    class Blog{
-        private $conn;
-        private $table = "blogs";
+class Blog
+{
+    private $conn;
+    private $table = "blogs";
 
-        public function __construct($db){
-            $this->conn = $db;
-        }
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 
-        public function createBlog($details){
-            boolval($details['isDraft']) ? $isDraft = 1 : $isDraft = 0;
-            $tags = $details['tags'] ? serialize($details['tags']) : serialize([]);
+    public function createBlog($details)
+    {
+        boolval($details['isDraft']) ? $isDraft = 1 : $isDraft = 0;
+        $tags = $details['tags'] ? serialize($details['tags']) : serialize([]);
 
-            $query =
-                "INSERT INTO $this->table
+        $query =
+            "INSERT INTO $this->table
                 (userEmail, userName, blogTitle, content, tags, isDraft)
                 VALUES (
                     '$details[userEmail]',
@@ -22,27 +25,29 @@
                     '$details[content]',
                     '$tags',
                     '$isDraft'
-                )";                
-            $res = $this->conn->query($query);
-            return $res;
-        }
+                )";
+        $res = $this->conn->query($query);
+        return $res;
+    }
 
-        public function fetchAllBlogs($userEmail){
-            $query = 
-                "SELECT * FROM $this->table 
+    public function fetchAllBlogs($userEmail)
+    {
+        $query =
+            "SELECT * FROM $this->table 
                 WHERE userEmail = '$userEmail'";
-            
-            $res = $this->conn->query($query);            
-            return $res;
-        }
 
-        public function updateBlog($details){
-            boolval($details['isDraft']) ? $isDraft = 1 : $isDraft = 0;
-            boolval($details['isPublished']) ? $isPublishing = 0 : ($isDraft ? $isPublishing = 0 : $isPublishing = 1);
-            $tags = $details['tags'] ? serialize($details['tags']) : serialize([]);
+        $res = $this->conn->query($query);
+        return $res;
+    }
 
-            $query = $isPublishing ? 
-                    "UPDATE $this->table
+    public function updateBlog($details)
+    {
+        boolval($details['isDraft']) ? $isDraft = 1 : $isDraft = 0;
+        boolval($details['isPublished']) ? $isPublishing = 0 : ($isDraft ? $isPublishing = 0 : $isPublishing = 1);
+        $tags = $details['tags'] ? serialize($details['tags']) : serialize([]);
+
+        $query = $isPublishing ?
+            "UPDATE $this->table
                     SET 
                         blogTitle = '$details[blogTitle]',
                         content = '$details[content]',
@@ -50,16 +55,26 @@
                         isDraft = '$isDraft',
                         published = (SELECT CURRENT_TIMESTAMP)
                     WHERE userEmail = '$details[userEmail]' AND blogId = '$details[blogId]'"
-                    :
-                    "UPDATE $this->table
+            :
+            "UPDATE $this->table
                     SET 
                         blogTitle = '$details[blogTitle]',
                         content = '$details[content]',
                         tags = '$tags',
                         isDraft = '$isDraft'
                     WHERE userEmail = '$details[userEmail]' AND blogId = '$details[blogId]'";
-            
-            $res = $this->conn->query($query);
-            return $res;
-        }
+
+        $res = $this->conn->query($query);
+        return $res;
     }
+
+    public function deleteBlog($id)
+    {
+        $query =
+            "DELETE FROM $this->table 
+                WHERE blogId = '$id'";
+
+        $res = $this->conn->query($query);
+        return $res;
+    }
+}
