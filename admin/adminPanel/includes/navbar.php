f349b42115d7e33ff86aca4117f50586db4cc119
priyanshuuuuu <anshu.rohilla.pr@gmail.com>
2021-09-21 23:18:44 +0530

    dashboard minor bug fix

--- admin/adminPanel/includes/navbar.php
+++ admin/adminPanel/includes/navbar.php
@@ -14,7 +14,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
