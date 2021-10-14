d458d8bdd369a6e45f9fb6054f6fcc1ea00ded53
Kashish Jain <kjcoder04@gmail.com>
2021-10-14 23:05:56 +0530

    react build

--- user/dashboard/frontend/src/pages/Auth/Login.js
+++ user/dashboard/frontend/src/pages/Auth/Login.js
@@ -21,100 +21,100 @@ import { login } from "api/user";
 
 // reactstrap components
 import {
-  Button,
-  Card,
-  CardBody,
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Row,
-  Col,
+	Button,
+	Card,
+	CardBody,
+	FormGroup,
+	Form,
+	Input,
+	InputGroupAddon,
+	InputGroupText,
+	InputGroup,
+	Row,
+	Col,
+	Spinner,
 } from "reactstrap";
 
 const Login = () => {
-  const dispatch = useDispatch(),
-    [email, setEmail] = useState(""),
-    [password, setPassword] = useState("");
+	const dispatch = useDispatch(),
+		[email, setEmail] = useState(""),
+		[password, setPassword] = useState(""),
+		[loading, setLoading] = useState(false);
 
-  const loginHandler = (e) => {
-    e.preventDefault();
-    if (email === "" || password === "") {
-      return alert("Please fill all the required fields.");
-    }
-    const body = {
-      email,
-      password,
-    };
-    dispatch(login(body));
-  };
+	const loginHandler = async (e) => {
+		e.preventDefault();
+		if (email === "" || password === "") {
+			return alert("Please fill all the required fields.");
+		}
+		setLoading(true);
+		const body = {
+			email,
+			password,
+		};
+    setTimeout(async () => {
+      dispatch(login(body, setLoading));
+    }, 2000);		
+	};
 
-  return (
-    <div className="loginPage">
-      <Card className="col-lg-4 col-md-7 mt-5 bg-secondary shadow border-0 glass">
-        <CardBody className="px-lg-5 py-lg-5">
-          <div className="text-center text-muted mb-4">
-            <small>Please enter your credentials</small>
-          </div>
-          <Form onSubmit={loginHandler} role="form">
-            <FormGroup className="mb-3">
-              <InputGroup className="input-group-alternative">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="ni ni-email-83" />
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input
-                  placeholder="Email"
-                  type="email"
-                  autoComplete="new-email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </InputGroup>
-            </FormGroup>
-            <FormGroup>
-              <InputGroup className="input-group-alternative">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="ni ni-lock-circle-open" />
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input
-                  placeholder="Password"
-                  type="password"
-                  autoComplete="new-password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </InputGroup>
-            </FormGroup>
-            <div className="text-center">
-              <Button
-                className="my-4"
-                color="primary"
-                type="submit"
-                onClick={loginHandler}
-              >
-                Sign in
-              </Button>
-            </div>
-          </Form>
-        </CardBody>
-      </Card>
-      <Row className="mt-3">
-        <Col className="text-right" xs="12">
-          <a className="text-light" href="/register">
-            <small>Create new account</small>
-          </a>
-        </Col>
-      </Row>
-    </div>
-  );
+	return (
+		<div className="loginPage">
+			<Card className="col-lg-4 col-md-7 mt-5 bg-secondary shadow border-0 glass">
+				<CardBody className="px-lg-5 py-lg-5">
+					<div className="text-center text-muted mb-4">
+						<small>Please enter your credentials</small>
+					</div>
+					<Form onSubmit={loginHandler} role="form">
+						<FormGroup className="mb-3">
+							<InputGroup className="input-group-alternative">
+								<InputGroupAddon addonType="prepend">
+									<InputGroupText>
+										<i className="ni ni-email-83" />
+									</InputGroupText>
+								</InputGroupAddon>
+								<Input
+									placeholder="Email"
+									type="email"
+									autoComplete="new-email"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+									required
+								/>
+							</InputGroup>
+						</FormGroup>
+						<FormGroup>
+							<InputGroup className="input-group-alternative">
+								<InputGroupAddon addonType="prepend">
+									<InputGroupText>
+										<i className="ni ni-lock-circle-open" />
+									</InputGroupText>
+								</InputGroupAddon>
+								<Input
+									placeholder="Password"
+									type="password"
+									autoComplete="new-password"
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									required
+								/>
+							</InputGroup>
+						</FormGroup>
+						<div className="text-center">
+							<Button className="my-4 w-100" color="primary" type="submit">
+								{loading ? <Spinner color="light" size="sm" /> : "Sign in"}
+							</Button>
+						</div>
+					</Form>
+				</CardBody>
+			</Card>
+			<Row className="mt-3">
+				<Col className="text-right" xs="12">
+					<a className="text-light" href="/register">
+						<small>Create new account</small>
+					</a>
+				</Col>
+			</Row>
+		</div>
+	);
 };
 
 export default Login;
