f98e1f146e866d3300f31333c5a3d0cc702e241f
mayurpuri25 <79853437+mayurpuri25@users.noreply.github.com>
2021-10-16 14:57:29 +0530

    winner section updated

--- dashboard/frontend/src/components/Dashboard/AllWinner.js
+++ dashboard/frontend/src/components/Dashboard/AllWinner.js
@@ -1,27 +1,27 @@
 const winner = [
   {
     winId: 1,
-    name: "Captain America",
-    eventName: "Civil War",
-    img: "https://static2.srcdn.com/wordpress/wp-content/uploads/2018/06/Captain-America-laughing.jpg"
+    name: "NAME ",
+    eventName: "EVENT NAME",
+    img:"https://www.unmc.edu/cihc/_images/faculty/default.jpg" 
   },
   {
     winId: 2,
-    name: "Thanos",
-    eventName: "Infinity War",
-    img:"https://i.pinimg.com/736x/fb/1f/c2/fb1fc258463a3c3c567ee660e40affe6.jpg"
+    name: "NAME ",
+    eventName: "EVENT NAME",
+    img:"https://www.unmc.edu/cihc/_images/faculty/default.jpg"
   },
   {
     winId: 3,
-    name: "Deadpool",
-    eventName: "****",
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnE2T94kYF_HYdeUL6NSzKu350aAJ8xvgaC2eMZeAY-JceCx6WV0REXEZedpldgNHv83s&usqp=CAU"
+    name: "NAME ",
+    eventName: "EVENT NAME",
+    img:"https://www.unmc.edu/cihc/_images/faculty/default.jpg"
   },
   {
     winId: 4,
-    name: "Loki",
-    eventName: "betrayal",
-    img:"https://i.pinimg.com/474x/55/39/be/5539be0c096a1c74803168b7b0a8db91.jpg"
+    name: "NAME ",
+    eventName: "EVENT NAME",
+    img:"https://www.unmc.edu/cihc/_images/faculty/default.jpg"
   },
 ];
 
