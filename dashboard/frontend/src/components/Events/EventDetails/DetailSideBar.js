eac2da28ffeb54207710e479d5a5d7f4b1ff2606
Kashish Jain <kjcoder04@gmail.com>
2021-10-17 11:53:20 +0530

    events backend

--- dashboard/frontend/src/components/Events/EventDetails/DetailSideBar.js
+++ dashboard/frontend/src/components/Events/EventDetails/DetailSideBar.js
@@ -3,34 +3,45 @@ import { Button } from "reactstrap";
 import "../../../assets/css/events/eventDetails.css";
 
 export default function DetailSidebar({ event }) {
-  return (
-    <>
-      <div className="detailSideBar ">
-        <h1>{event?.heading} </h1>
-        <hr />
-        <div className="detailContainer">
-          <div className="dateAndTime ">
-            <p>
-              <i className="ni ni-calendar-grid-58 "></i>
-              <span>Starts on : {event?.date} </span>
-            </p>
+	return (
+		<>
+			<div className="detailSideBar">
+				<h1>{event?.eventName} </h1>
+				<hr />
+				<div className="detailContainer">
+					<p className="detailContainer__description">
+						{event?.eventDescription}
+					</p>
+					<div className="dateAndTime ">
+						<p>
+							<i className="ni ni-calendar-grid-58 "></i>
+							<span>Starts : {event?.startDate} </span>
+						</p>
+						<p>
+							<i className="ni ni-calendar-grid-58 "></i>
+							<span>Ends : {event?.endDate} </span>
+						</p>
+						{event?.time && (
+							<p>
+								<i className="bx bx-time "></i>
+								<span>Time : {event?.time} </span>
+							</p>
+						)}
+					</div>
 
-            <p>
-              <i className="bx bx-time "></i>
-              <span>Time : {event?.time} </span>
-            </p>
-
-            <p>
-              <i className="bx bxs-map "></i>
-              <span>Venue : {event?.venue}</span>
-            </p>
-          </div>
-
-          <div className="text-right ">
-            <Button color="info">Register Now</Button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+					<div className="buttons text-right">
+						{event?.regLink && (
+							<a
+								href={event?.regLink}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Button color="info">Register Now</Button>
+							</a>
+						)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
