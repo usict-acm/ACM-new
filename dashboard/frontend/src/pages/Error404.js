9ccb5d885b766a2f5e24ffec8c189e08b8b688ea
ajit kumar <ajitkumar12105@gmail.com>
2021-11-24 22:52:06 +0530

    added 404 page

---
+++ dashboard/frontend/src/pages/Error404.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import "../assets/css/error404.css"
+
+const Error404 = () => {
+    return (
+        <>
+              {/* <!-- Content Wrapper --> */}
+    <div id="wrapper">
+      <div className="container">
+        {/* <!-- brick of wall --> */}
+        <div className="brick"></div>
+        {/* <!-- end brick of wall --> */}
+
+        {/* <!-- Number --> */}
+        <div className="number">
+          <div className="four"></div>
+          <div className="zero">
+            <div className="nail"></div>
+          </div>
+          <div className="four"></div>
+        </div>
+        {/* <!-- end Number --> */}
+
+        {/* <!-- Info --> */}
+        <div className="info">
+          <h2>Something is wrong</h2>
+          <p>
+            The page you are looking for was moved, removed, renamed or might
+            never existed.
+          </p>
+          <a href="./" className="btn btn-home">Go Home</a>
+        </div>
+        {/* <!-- end Info --> */}
+      </div>
+      {/* <!-- end container --> */}
+    </div>
+    {/* <!-- end Content Wrapper --> */}
+
+    <div id="foot-img">
+      <div className="container">
+        {/* <!-- Worker --> */}
+        <div className="worker"></div>
+      </div>
+      {/* <!-- end container --> */}
+    </div>
+
+        </>
+    )
+}
+
+export default Error404;
