868b2a077b541f2df76d60f0b99efb398eb2497c
mayurpuri25 <79853437+mayurpuri25@users.noreply.github.com>
2021-11-25 20:59:51 +0530

    bem convention on Error404.css

--- dashboard/frontend/src/pages/Error404.js
+++ dashboard/frontend/src/pages/Error404.js
@@ -1,51 +1,52 @@
-import React from 'react'
-import "../assets/css/error404.css"
+import React from "react";
+import "../assets/css/error404.css";
 
 const Error404 = () => {
-    return (
-        <div className="Error_404">
-              {/* <!-- Content Wrapper --> */}
-    <div id="wrapper">
-      <div className="container">
-        {/* <!-- brick of wall --> */}
-        <div className="brick"></div>
-        {/* <!-- end brick of wall --> */}
+  return (
+    <div className="Error_404">
+      {/* <!-- Content Wrapper --> */}
+      <div className="Error_404__wrapper">
+        <div className="container Error_404__container">
+          {/* <!-- brick of wall --> */}
+          <div className="Error_404__brick"></div>
+          {/* <!-- end brick of wall --> */}
 
-        {/* <!-- Number --> */}
-        <div className="number">
-          <div className="four"></div>
-          <div className="zero">
-            <div className="nail"></div>
+          {/* <!-- Number --> */}
+          <div className="Error_404__number">
+            <div className="Error_404__four"></div>
+            <div className="Error_404__zero">
+              <div className="Error_404__nail"></div>
+            </div>
+            <div className="Error_404__four"></div>
           </div>
-          <div className="four"></div>
-        </div>
-        {/* <!-- end Number --> */}
+          {/* <!-- end Number --> */}
 
-        {/* <!-- Info --> */}
-        <div className="info">
-          <h2>Something is wrong</h2>
-          <p>
-            The page you are looking for was moved, removed, renamed or might
-            never existed.
-          </p>
-          <a href="./" className="btn btn-home">Go Home</a>
+          {/* <!-- Info --> */}
+          <div className="Error_404__info">
+            <h2 className="Error_404__info-h2">Something is wrong</h2>
+            <p className="Error_404__info-p">
+              The page you are looking for was moved, removed, renamed or might
+              never existed.
+            </p>
+            <a href="./" className="btn Error_404__info-btn Error_404__info--a">
+              Go Home
+            </a>
+          </div>
+          {/* <!-- end Info --> */}
         </div>
-        {/* <!-- end Info --> */}
+        {/* <!-- end container --> */}
       </div>
-      {/* <!-- end container --> */}
-    </div>
-    {/* <!-- end Content Wrapper --> */}
+      {/* <!-- end Content Wrapper --> */}
 
-    <div id="foot-img">
-      <div className="container">
-        {/* <!-- Worker --> */}
-        <div className="worker"></div>
-      </div>
-      {/* <!-- end container --> */}
-    </div>
-
+      <div className="Error_404__foot-img">
+        <div className="container Error_404__container">
+          {/* <!-- Worker --> */}
+          <div className="Error_404__worker"></div>
         </div>
-    )
-}
+        {/* <!-- end container --> */}
+      </div>
+    </div>
+  );
+};
 
 export default Error404;
