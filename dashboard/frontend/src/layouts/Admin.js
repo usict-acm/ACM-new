da5ca4d81c3f6ba2c59708c6ad1b5aaf2c14e6d9
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-11-26 12:04:03 +0530

    404 route setup

--- dashboard/frontend/src/layouts/Admin.js
+++ dashboard/frontend/src/layouts/Admin.js
@@ -1,98 +1,100 @@
-import React from "react";
-import { useLocation, Route, Switch, Redirect } from "react-router-dom";
-import { Container } from "reactstrap";
-import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import AdminFooter from "components/Footers/AdminFooter.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
+import React from 'react'
+import { useLocation, Route, Switch, Redirect } from 'react-router-dom'
+import { Container } from 'reactstrap'
+import AdminNavbar from 'components/Navbars/AdminNavbar.js'
+import AdminFooter from 'components/Footers/AdminFooter.js'
+import Sidebar from 'components/Sidebar/Sidebar.js'
 
-import routes from "routes.js";
-import CreateBlog from "pages/Admin/CreateBlog";
-import DisplayBlog from "pages/Admin/DisplayBlog";
-import EventDetails from "pages/Admin/Events/EventDetails";
+import routes from 'routes.js'
+import CreateBlog from 'pages/Admin/CreateBlog'
+import DisplayBlog from 'pages/Admin/DisplayBlog'
+import EventDetails from 'pages/Admin/Events/EventDetails'
+import Error404 from 'pages/Error404'
 
 const Admin = (props) => {
-	const mainContent = React.useRef(null);
-	const location = useLocation();
+  const mainContent = React.useRef(null)
+  const location = useLocation()
 
-	React.useEffect(() => {
-		document.documentElement.scrollTop = 0;
-		document.scrollingElement.scrollTop = 0;
-		if(mainContent?.current) mainContent.current.scrollTop = 0;
-	}, [location]);
+  React.useEffect(() => {
+    document.documentElement.scrollTop = 0
+    document.scrollingElement.scrollTop = 0
+    if (mainContent?.current) mainContent.current.scrollTop = 0
+  }, [location])
 
-	const getBrandText = () => {
-		for (let i = 0; i < routes.length; i++) {
-			if (location.pathname === routes[i].path) return routes[i].name;
-		}
-		return "";
-	};
+  const getBrandText = () => {
+    for (let i = 0; i < routes.length; i++) {
+      if (location.pathname === routes[i].path) return routes[i].name
+    }
+    return ''
+  }
 
-	const getRoutes = (routes) => {
-		return routes.map((prop, key) => {
-			if (prop.layout === "/admin") {
-				return (
-					<Route exact path={prop.path} key={key}>
-						<Sidebar
-							{...props}
-							routes={routes}
-							logo={{
-								innerLink: "/home",
-								imgSrc: require("../assets/img/brand/argon-react.png").default,
-								imgAlt: "...",
-							}}
-						/>
-						<div className="main-content" ref={mainContent}>
-							<AdminNavbar
-								{...props}
-								brandText={getBrandText(location.pathname)}
-							/>
-							<prop.component />
-							<Container fluid>
-								<AdminFooter />
-							</Container>
-						</div>
-					</Route>
-				);
-			} else {
-				return null;
-			}
-		});
-	};
+  const getRoutes = (routes) => {
+    return routes.map((prop, key) => {
+      if (prop.layout === '/admin') {
+        return (
+          <Route exact path={prop.path} key={key}>
+            <Sidebar
+              {...props}
+              routes={routes}
+              logo={{
+                innerLink: '/home',
+                imgSrc: require('../assets/img/brand/argon-react.png').default,
+                imgAlt: '...',
+              }}
+            />
+            <div className='main-content' ref={mainContent}>
+              <AdminNavbar
+                {...props}
+                brandText={getBrandText(location.pathname)}
+              />
+              <prop.component />
+              <Container fluid>
+                <AdminFooter />
+              </Container>
+            </div>
+          </Route>
+        )
+      } else {
+        return null
+      }
+    })
+  }
 
-	const RenderRoute = (details) => (
-		<Route exact path={details.path}>
-			<Sidebar
-				{...props}
-				routes={routes}
-				logo={{
-					innerLink: "/home",
-					imgSrc: require("../assets/img/brand/argon-react.png").default,
-					imgAlt: "...",
-				}}
-			/>
-			<div className="main-content" ref={mainContent}>
-				<AdminNavbar {...props} brandText={getBrandText(location.pathname)} />
-				<details.component />
-				<Container fluid>
-					<AdminFooter />
-				</Container>
-			</div>
-		</Route>
-	);
+  const RenderRoute = (details) => (
+    <Route exact path={details.path}>
+      <Sidebar
+        {...props}
+        routes={routes}
+        logo={{
+          innerLink: '/home',
+          imgSrc: require('../assets/img/brand/argon-react.png').default,
+          imgAlt: '...',
+        }}
+      />
+      <div className='main-content' ref={mainContent}>
+        <AdminNavbar {...props} brandText={getBrandText(location.pathname)} />
+        <details.component />
+        <Container fluid>
+          <AdminFooter />
+        </Container>
+      </div>
+    </Route>
+  )
 
-	return (
-		<Switch>
-			{getRoutes(routes)}
-			<RenderRoute path="/blog/:blogId" component={DisplayBlog} />
-			<RenderRoute path="/createBlog/:blogId" component={CreateBlog} />
-			<RenderRoute path="/createBlog" component={CreateBlog} />
-			<RenderRoute path="/event/:eventId" component={EventDetails} />			
-			<Redirect exact from="/" to="/home" />
-			<Redirect exact from="/login" to="/home" />
-			<Redirect exact from="/register" to="/home" />
-			<Redirect from="*" to="/404" />
-		</Switch>
-	);
-};
+  return (
+    <Switch>
+      {getRoutes(routes)}
+      <RenderRoute path='/blog/:blogId' component={DisplayBlog} />
+      <RenderRoute path='/createBlog/:blogId' component={CreateBlog} />
+      <RenderRoute path='/createBlog' component={CreateBlog} />
+      <RenderRoute path='/event/:eventId' component={EventDetails} />
+      <Redirect exact from='/' to='/home' />
+      <Redirect exact from='/login' to='/home' />
+      <Redirect exact from='/register' to='/home' />
+      <Route path='/404' component={Error404} />
+      <Redirect from='*' to='/404' />
+    </Switch>
+  )
+}
 
-export default Admin;
+export default Admin
