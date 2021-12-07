31e6fe8993e339d45a18808120752db8c2a996d1
mayurpuri25 <79853437+mayurpuri25@users.noreply.github.com>
2021-12-07 11:38:20 +0530

    password limit and new fields - register page

--- dashboard/frontend/src/utils/Constants.js
+++ dashboard/frontend/src/utils/Constants.js
@@ -1,13 +1,15 @@
 export const BRANCHES = {
-  "B.Tech.": ["CSE", "IT", "ECE"],
-  "M.Tech.": ["CSE", "IT", "ECE"],
-  MCA: ["Software Engineering"],
+  "B.Tech.": ["CSE", "IT", "ECE","Other"],
+  "M.Tech.": ["CSE", "IT", "ECE","Other"],
+  "MCA": ["Software Engineering","Other"],
+  "Other":["Other"]
 };
-
+ 
 export const COLLEGES = [
   "University School of Information, Communication & Technology (USICT)",
   "University School of Management Studies (USMS)",
   "University School of Automation & Robotics (USAR)",
   "University School of Design & Innovation, (USDI)",
   "IP Innovation & Incubation Center",
+  "Other"
 ];
