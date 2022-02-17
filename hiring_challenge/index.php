029303c6899f8afb8c7b88da7411e8da429899ab
Rithik Sharma <srithik645@gmail.com>
2022-02-17 22:43:27 +0530

    Home Section

--- hiring_challenge/index.php
+++ hiring_challenge/index.php
@@ -7,13 +7,13 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <link href="https://fonts.googleapis.com/css?family=Vollkorn:400,400i,600,700,900&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="style.css" />
-  <title>The Road</title>
+  <title>ACM HACKATHON</title>
 </head>
 
 <body>
-
-  <div class="container">
+  <section class="container">
     <!-- Header -->
+
     <div class="area">
       <ul class="circles">
         <li></li>
@@ -27,56 +27,58 @@
         <li></li>
         <li></li>
       </ul>
-      <header class="header center">
-        <div class="header-text">
-          <h1 class="heading">TRELLATHON</h1>
-          <p class="header-paragraph">
-            24-HOUR HACKATHON
-            9TH MARCH 2021
-          </p>
+
+
+
+      <nav id="navbar">
+        <div class="acm-logo">
+          <img src="images/logo-acm.png" alt="" class="acm-img">
         </div>
+        <ul>
+          <li class="item"><a href="#about">About</a></li>
+          <li class="item"><a href="#timeline">Timeline</a></li>
+          <li class="item"><a href="#themes">Themes</a></li>
+          <li class="item"><a href="#partners">Partners</a></li>
+          <li class="item"><a href="#team">Team</a></li>
+          <li class="item"><a href="#faq">FAQ</a></li>
+          <li class="item"><a href="#contact">Contact</a></li>
+        </ul>
 
-        <nav id="navbar">
-          <div class="acm-logo">
-            <img src="acm1.png" alt="" class="acm-img">
-          </div>
-          <ul>
-            <li class="item"><a href="#about">About</a></li>
-            <li class="item"><a href="#timeline">Timeline</a></li>
-            <li class="item"><a href="#themes">Themes</a></li>
-            <li class="item"><a href="#partners">Partners</a></li>
-            <li class="item"><a href="#team">Team</a></li>
-            <li class="item"><a href="#faq">FAQ</a></li>
-            <li class="item"><a href="#contact">Contact</a></li>
-          </ul>
+        <div class="logo">
+          <h1>
+            <span class="center">H</span>
+            <span class="center">a</span>
+            <span class="center">c</span>
+            <span class="center">k</span>
+            <span class="center">a</span>
+            <span class="center">t</span>
+            <span class="center">h</span>
+            <span class="center">o</span>
+            <span class="center">n</span>
+          </h1>
+        </div>
+      </nav>
 
-          <div class="logo">
-            <h1>
-              <span class="center">H</span>
-              <span class="center">a</span>
-              <span class="center">c</span>
-              <span class="center">k</span>
-              <span class="center">a</span>
-              <span class="center">t</span>
-              <span class="center">h</span>
-              <span class="center">o</span>
-              <span class="center">n</span>
-            </h1>
-          </div>
-        </nav>
 
-      </header>
-
+      <div class="header-text">
+        <h1 class="heading">TRELLATHON</h1>
+        <p class="header-paragraph">
+          24-HOUR HACKATHON
+          9TH MARCH 2021
+        </p>
+      </div>
     </div>
     <!-- End of Header -->
 
+    
+<!-- start of About Section -->
 
 
 
 
-  </div>
+
+  </section>
   <script src="script.js"></script>
- 
 </body>
 
 </html>
