ca654a9d38eb668528029422173b5597070dd3df
Manik Aggarwal <manikaggarwal993@gmail.com>
2022-02-19 16:36:06 +0530

    Navbar and animation added

--- hiring_challenge/index.php
+++ hiring_challenge/index.php
@@ -7,11 +7,13 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <link href="https://fonts.googleapis.com/css?family=Vollkorn:400,400i,600,700,900&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="style.css" />
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>ACM HACKATHON</title>
 </head>
 
 <body>
-  <section class="container">
+  <section class="fluid-container">
     <!-- Header -->
 
     <div class="area">
@@ -31,34 +33,31 @@
 
 
       <nav id="navbar">
-        <div class="acm-logo">
-          <img src="acm1.png" alt="" class="acm-img">
-        </div>
-        <ul>
-          <li class="item"><a href="#about">About</a></li>
-          <li class="item"><a href="#timeline">Timeline</a></li>
-          <li class="item"><a href="#themes">Themes</a></li>
-          <li class="item"><a href="#partners">Partners</a></li>
-          <li class="item"><a href="#team">Team</a></li>
-          <li class="item"><a href="#faq">FAQ</a></li>
-          <li class="item"><a href="#contact">Contact</a></li>
-        </ul>
-
-        <div class="logo">
-          <h1>
-            <span class="center">H</span>
-            <span class="center">a</span>
-            <span class="center">c</span>
-            <span class="center">k</span>
-            <span class="center">a</span>
-            <span class="center">t</span>
-            <span class="center">h</span>
-            <span class="center">o</span>
-            <span class="center">n</span>
-          </h1>
-        </div>
+          <div id="acmimg"><img src="acm1.png" alt="" class="acm-img"></div>
+          <ul>
+            <li class="item"><a href="#about">About</a></li>
+            <li class="item"><a href="#timeline">Timeline</a></li>
+            <li class="item"><a href="#themes">Themes</a></li>
+            <li class="item"><a href="#partners">Partners</a></li>
+            <li class="item"><a href="#team">Team</a></li>
+            <li class="item"><a href="#faq">FAQ</a></li>
+            <li class="item"><a href="#contact">Contact</a></li>
+          </ul>
       </nav>
 
+      <div class="logo">
+          <h1>
+            <span class="text-center pt-2">H</span>
+            <span class="text-center pt-2">a</span>
+            <span class="text-center pt-2">c</span>
+            <span class="text-center pt-2">k</span>
+            <span class="text-center pt-2">a</span>
+            <span class="text-center pt-2">t</span>
+            <span class="text-center pt-2">h</span>
+            <span class="text-center pt-2">o</span>
+            <span class="text-center pt-2">n</span>
+          </h1>
+        </div>
 
       <div class="header-text">
         <h1 class="heading">TRELLATHON</h1>
@@ -69,15 +68,19 @@
       </div>
     </div>
     <!-- End of Header -->
-
-
-<!-- start of About Section -->
-
-
-
-
-
   </section>
+
+  <section id="about">
+    <div>
+      <h1>About US</h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, laborum culpa. Ab quibusdam repellat repellendus soluta ea hic eligendi minus perspiciatis inventore? Vero, accusantium. Voluptate laborum ipsa explicabo quidem ipsum?
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, laborum culpa. Ab quibusdam repellat repellendus soluta ea hic eligendi minus perspiciatis inven
+    </div>
+    </section>
+    
+
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="script.js"></script>
 </body>
 
