e6a97492469170b7e2e0a48574f8bdbe8764dc1c
Manik Aggarwal <manikaggarwal993@gmail.com>
2022-02-20 17:05:58 +0530

    Team and footer section  added

--- hiring_challenge/index.php
+++ hiring_challenge/index.php
@@ -2,91 +2,350 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <link href="https://fonts.googleapis.com/css?family=Vollkorn:400,400i,600,700,900&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="style.css" />
-  
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <title>ACM HACKATHON</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link href="https://fonts.googleapis.com/css?family=Vollkorn:400,400i,600,700,900&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>ACM HACKATHON</title>
 </head>
 
 <body>
-  <section class="fluid-container">
     <!-- Header -->
+    <section class="fluid-container">
+        <div class="area">
+            <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
 
-    <div class="area">
-      <ul class="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+                <!-- NAVBAR -->
+            <nav id="navbar">
+                    <div class='test'>
+                        <div id="acmimg"><img src="acm1.png" alt="" class="acm-img"></div>
+                        <div class="logo">
+                            <h1>
+                                <span class="text-center pt-2">H</span>
+                                <span class="text-center pt-2">a</span>
+                                <span class="text-center pt-2">c</span>
+                                <span class="text-center pt-2">k</span>
+                                <span class="text-center pt-2">a</span>
+                                <span class="text-center pt-2">t</span>
+                                <span class="text-center pt-2">h</span>
+                                <span class="text-center pt-2">o</span>
+                                <span class="text-center pt-2">n</span>
+                            </h1>
+                        </div>
+                    </div>
 
+                    <ul>
+                        <li class="item"><a href="#about">About</a></li>
+                        <li class="item"><a href="#timeline">Timeline</a></li>
+                        <li class="item"><a href="#themes">Themes</a></li>
+                        <li class="item"><a href="#partners">Partners</a></li>
+                        <li class="item"><a href="#team">Team</a></li>
+                        <li class="item"><a href="#faq">FAQ</a></li>
+                        <li class="item"><a href="#contact">Contact</a></li>
+                    </ul>
+            </nav>
+                <!-- NAVBAR -->
 
+            <div class="header-text">
+                <h1 class="heading">TRELLATHON</h1>
+                <p class="header-paragraph">
+                    24-HOUR HACKATHON
+                    9TH MARCH 2021
+                </p>
+            </div>
+        </div>
+    </section>
+    <!-- End of Header -->
 
-      <nav id="navbar">
-        <div class='test'>
-          <div id="acmimg"><img src="acm1.png" alt="" class="acm-img"></div>
-          <div class="logo">
-            <h1>
-              <span class="text-center pt-2">H</span>
-              <span class="text-center pt-2">a</span>
-              <span class="text-center pt-2">c</span>
-              <span class="text-center pt-2">k</span>
-              <span class="text-center pt-2">a</span>
-              <span class="text-center pt-2">t</span>
-              <span class="text-center pt-2">h</span>
-              <span class="text-center pt-2">o</span>
-              <span class="text-center pt-2">n</span>
-            </h1>
-          </div>
+    <!-- ABOUT -->
+    <section class="about-section">
+        <div class="inner-container">
+            <h1>About Us</h1>
+            <p class="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus, enim inventore earum,
+                eligendi nostrum pariatur necessitatibus eius dicta a voluptates sit deleniti autem error eos totam nisi
+                neque voluptates sit deleniti autem error eos totam nisi neque.
+            </p>
+            <div class="skills">
+                <span>Web Design</span>
+                <span>Photoshop & Illustrator</span>
+                <span>Coding</span>
+            </div>
+        </div>
+    </section>
+    <!-- ABOUT -->
+
+    <!-- TEAM -->
+    <div class="container">
+        <h1 class="heading">Our Team</h1>
+        <div class="hr">
+            <hr>
         </div>
 
-          <ul>
-            <li class="item"><a href="#about">About</a></li>
-            <li class="item"><a href="#timeline">Timeline</a></li>
-            <li class="item"><a href="#themes">Themes</a></li>
-            <li class="item"><a href="#partners">Partners</a></li>
-            <li class="item"><a href="#team">Team</a></li>
-            <li class="item"><a href="#faq">FAQ</a></li>
-            <li class="item"><a href="#contact">Contact</a></li>
-          </ul>
-      </nav>
+        <div class="row">
+            <div class="col-md-2 col-sm-6 box-1">
+                <div class="our-team">
+                    <div class="pic"><img class="image" src="image4-modified.png" alt=""></div>
+                    <h3 class="title">Williamson</h3>
+                    <span class="post">Web Developer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
 
-      
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
 
-      <div class="header-text">
-        <h1 class="heading">TRELLATHON</h1>
-        <p class="header-paragraph">
-          24-HOUR HACKATHON
-          9TH MARCH 2021
-        </p>
-      </div>
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-2 col-sm-6 box-1">
+                <div class="our-team">
+                    <div class="pic"><img class="image" src="image4-modified.png" alt=""></div>
+                    <h3 class="title">Williamson</h3>
+                    <span class="post">Web Developer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6">
+                <div class="our-team">
+                    <div class="pic"><img class="image"
+                            src="https://image.shutterstock.com/image-vector/cute-kid-teen-boy-show-260nw-1509139481.jpg"
+                            alt=""></div>
+                    <h3 class="title">Kristiana</h3>
+                    <span class="post">Web Designer</span>
+                    <ul class="icon">
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-linkedin"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+
     </div>
-    <!-- End of Header -->
-  </section>
+    <!-- TEAM -->
 
-  <section id="about">
-    <div>
-      <h1>About US</h1>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, laborum culpa. Ab quibusdam repellat repellendus soluta ea hic eligendi minus perspiciatis inventore? Vero, accusantium. Voluptate laborum ipsa explicabo quidem ipsum?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, laborum culpa. Ab quibusdam repellat repellendus soluta ea hic eligendi minus perspiciatis inven
-    </div>
+    <!-- FOOTER -->
+    <section class="fluid-container mt-5 mb-3">
+        <div class="card bg-white mx-5">
+            <div class="row mb-4">
+                <div class="col-md-4 col-sm-4 col-xs-4">
+                    <div class="footer-text pull-left">
+                        <div class="d-flex">
+                            <img src="acm1.png" alt="" class="footer-acm">
+                        </div>
+                        <h1>USICT ACM Student Chapter</h1>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi non
+                            pariatur numquam animi nam at impedit odit nisi.</p>
+                        <div class="social mt-3 mb-3"> <i class="fa fa-facebook-official fa-lg"></i> <i
+                                class="fa fa-instagram fa-lg"></i> <i class="fa fa-twitter fa-lg"></i> <i
+                                class="fa fa-linkedin-square fa-lg"></i> <i class="fa fa-facebook"></i> </div>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-2"></div>
+                <div class="col-md-2 col-sm-2 col-xs-2">
+                    <h5 class="heading_footer">Quick Links</h5>
+                    <ul>
+                        <li class='footli'>Home</li>
+                        <li class='footli'>About</li>
+                        <li class='footli'>Team</li>
+                        <li class='footli'>FAQ</li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-2">
+                    <h5 class="heading_footer">Industries</h5>
+                    <ul class="card-text">
+                        <li class='footli'>Finance</li>
+                        <li class='footli'>Public Sector</li>
+                        <li class='footli'>Smart Office</li>
+                        <li class='footli'>Retail</li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-2">
+                    <h5 class="heading_footer">Company</h5>
+                    <ul class="card-text">
+                        <li class='footli'>About Us</li>
+                        <li class='footli'>Blog</li>
+                        <li class='footli'>Contact</li>
+                        <li class='footli'>Join Us</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="divider mb-4"> </div>
+
+            <div class="d-flex">
+                <p id="copyright" style="margin-left: 30rem;">© Conceptualized,Designed and Developed by Office Bearers
+                    of ACM USICT chapter.
+                </p>
+            </div>
+        </div>
     </section>
-    
+    <!-- FOOTER -->
 
-  
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  <script src="script.js"></script>
+
+    <script src="https://kit.fontawesome.com/833875f75e.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+    <script src="script.js"></script>
 </body>
 
 </html>
-
