63680cd8699452434b43c4f90a06f2e50a179ce0
sinha-toyeesh <toyeshsainik@gmail.com>
2022-02-24 14:25:31 +0530

    Added Timeline

--- hiring_challenge/index.php
+++ hiring_challenge/index.php
@@ -77,6 +77,157 @@
     </section>
     <!--**************************************************** END OF ABOUT *****************************************************-->
 
+    <!--****************************************************** TIMELINE *******************************************************-->
+
+    <div class="container-timeline">
+    <h1 class = "timeline-heading">Timeline</h1>
+    <div class="timeline">
+		<div class="timeline-row">
+			<div class="timeline-time">
+				7:45PM<small>Dec 21</small>
+			</div>
+			<div class="timeline-content">
+				<i class="icon-attachment"></i>
+				<h4>Admin theme!</h4>
+				<p>Maxwell Admin Dashboard contains C3 graphs, flot graphs, data tables, calendar, drag &amp; drop and ion slider.</p>
+				<!-- <div class="thumbs">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="Maxwell Admin">
+				</div> -->
+				<!-- <div class="">
+					<span class="badge badge-pill">Sales</span>
+					<span class="badge badge-pill">Admin</span>
+				</div> -->
+			</div>
+		</div>
+
+		<div class="timeline-row">
+			<div class="timeline-time">
+				8:00 AM<small>Dec 18</small>
+			</div>
+			<div class="timeline-content">
+				<i class="icon-code"></i>
+				<h4>Admin Dashboard!</h4>
+				<p>
+					Maxwell Admin Dashboard contains C3 graphs, flot graphs, data tables, calendar.
+				</p>
+				<!-- <div class="thumbs">
+					<div class="thumbs">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="Maxwell Admin">
+				</div> -->
+				<!-- </div> -->
+				<!-- <div class="">
+					<span class="badge badge-pill">Graphs</span>
+					<span class="badge badge-pill">Chat</span>
+				</div> -->
+			</div>
+		</div>
+
+		<div class="timeline-row">
+			<div class="timeline-time">
+				7:25 PM<small>Dec 6</small>
+			</div>
+			<div class="timeline-content">
+				<i class="icon-turned_in_not"></i>
+				<h4>Best Admin Template!</h4>
+				<p>Custom C3 graphs, Custom flot graphs, flot graphs, small graphs, Sass, profile and timeline.</p>
+				<!-- <div>
+					<span class="badge badge-pill">Invoice</span>
+					<span class="badge badge-pill">Tasks</span>
+				</div> -->
+			</div>
+		</div>
+
+		<div class="timeline-row">
+			<div class="timeline-time">
+				3:55 PM<small>Dec 26</small>
+			</div>
+			<div class="timeline-content">
+				<i class="icon-directions"></i>
+				<h4>Maxwell Admin</h4>
+				<p>Maxwell Admin Dashboard contains C3 graphs, flot graphs, data tables, calendar, drag &amp; drop and ion slider.</p>
+				<!-- <div>
+					<span class="badge badge-pill">Profile</span>
+					<span class="badge badge-pill">Sales</span>
+				</div> -->
+			</div>
+		</div>
+
+		<div class="timeline-row">
+			<div class="timeline-time">
+				5:24 PM<small>Dec 12</small>
+			</div>
+			<div class="timeline-content">
+				<i class="icon-change_history"></i>
+				<h4>Maxwell Dashboard</h4>
+				<p class="no-margin">Maxwell Admin dashboard includes invoice, profile, tasks, gallery, projects, maintanence.</p>
+				<!-- <div class="thumbs">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar5.png" alt="Maxwell Admin">
+				</div> -->
+				<!-- <div>
+					<span class="badge badge-pill">Analytics</span>
+					<span class="badge badge-pill">Contacts</span>
+				</div> -->
+			</div>
+		</div>
+
+		<div class="timeline-row">
+			<div class="timeline-time">
+				8:00 AM<small>Dec 18</small>
+			</div>
+			<div class="timeline-content">
+				<i class="icon-code"></i>
+				<h4>Admin Dashboard!</h4>
+				<p>
+					Maxwell Admin Dashboard contains C3 graphs, flot graphs, data tables, calendar.
+				</p>
+				<!-- <div class="thumbs">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar4.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="Maxwell Admin">
+				</div> -->
+				<!-- <div class="">
+					<span class="badge badge-pill">Ecommerce</span>
+					<span class="badge badge-pill">CRM</span>
+				</div> -->
+			</div>
+		</div>
+
+		<div class="timeline-row">
+			<div class="timeline-time">
+				7:45PM<small>Dec 21</small>
+			</div>
+			<div class="timeline-content">
+				<i class="icon-attachment"></i>
+				<h4>Admin Template!</h4>
+				<p>Maxwell Admin Dashboard contains C3 graphs, flot graphs, data tables, calendar, drag &amp; drop and ion slider.</p>
+				<!-- <div class="thumbs">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="Maxwell Admin">
+					<img class="img-fluid rounded" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="Maxwell Admin">
+				</div> -->
+				<!-- <div class="">
+					<span class="badge badge-pill">Orders</span>
+					<span class="badge badge-pill">Sales</span>
+				</div> -->
+			</div>
+		</div>
+
+		<div class="timeline-row">
+			<div class="timeline-content">
+				<p class="m-0">Loading...</p>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+    <!--****************************************************** END OF TIMELINE *******************************************************-->
+
     <!-- ************************************************* Start of Partner ************************************************* -->
     <section id="partner">
 
