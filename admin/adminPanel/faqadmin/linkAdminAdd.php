70504a8bb59e091adc2db8ea290ff24ef07c379a
nidhi-rathore <nidhirathore7022@gmail.com>
2022-02-25 20:32:47 +0530

    edit btn for already generated short link for given link

--- admin/adminPanel/faqadmin/linkAdminAdd.php
+++ admin/adminPanel/faqadmin/linkAdminAdd.php
@@ -50,6 +50,151 @@ $link = $database->connect();
 $connection = $link;
 
 /*                      *********           */
+if (isset($_POST['reg1'])) {
+  	$link_for = $_POST['inn1'];
+  	$original_link = $_POST['inn2'];
+      $s1Link='https://usict.acm.org/';
+  	$short_link = $_POST['inn3'];
+      $exx = explode(" ",$short_link);
+      $short_link = join("_",$exx);
+      $short_link=$s1Link.$short_link;
+    //   echo $short_link;
+    //  echo $original_link;
+    $sql_u = "SELECT * FROM link WHERE shortLink='$short_link'";
+    $res_u = mysqli_query($link, $sql_u);
+    if (preg_match('/[\'^£$%&*(}){@#~?><>,|=+¬]/', $short_link)){
+  
+            echo "<div class='wrapper'>
+            <div class='container-fluid'>
+                <div class='row'>
+                    <div class='col-md-12'>
+                        <div id='container'>
+                            <form method='POST' > 
+                                <div class='form-text new' style='display:inline-block;'>
+                                <div style='width: 700px; margin-left: 300px;'>
+                                <h6>Link For</h6>
+                                <input type='text' class='form-control ' value='".$link_for."' readonly /> <br>
+                    
+                                <h6>Original Link</h6>
+                                <input type='text' class='form-control ' value='".$original_link."' readonly />
+                                <br>
+                                            <input type='text' id='' style='position:absolute; width: 0px; height: 0px; margin-left: -300px;' name='inn1' value='".$link_for."' />
+                                            <input type='text' id='' style='position:absolute; width: 0px; height: 0px; margin-left: -300px;' name='inn2' value='".$original_link."' />
+                                                
+                                    <input type=text class='form-control' name='inn3' style='  padding:20px 5px 20px 180px; font-weight:100em; width:100%;' placeholder='Custom Link Name' id='inn3' required />
+                                    <label for='in3' class='static-value' style=' position:absolute;
+                                        color:white;
+                                        left:320px;
+                                        font-weight:bold;
+                                        font-size:1.1em;
+                                        color:#444;
+                                        top:190px;'>https://usict.acm.org/  
+                                    </label>
+                                </div> 
+                            </div>
+
+                                <br><br>
+                                <button type='submit' class='btn btn-primary' id='submitBtn' name='reg1' style='margin-left: 300px;' >Check & Confirm</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>";
+                    echo "<div style='position:absolute;margin:225px 0px 0px 255px; color:red; font-weight:bold;'>";
+                    echo "<h6 style=' margin-left:70px; font-size: 1rem;'>  Don't use special characters in custom name</h6>"  ;
+                    echo "</div>";
+        // echo "<br>";
+        // echo "<script>function copyAlGen(){navigator.clipboard.writeText('".$row['shortLink']."');}</script>";
+        // echo "<h6>Shorted Link</h6>";
+        // echo "<input type='text' class='form-control ' value='".$row['shortLink']."' readonly />";
+    }else if(mysqli_num_rows($res_u) > 0){
+        echo "<div class='wrapper'>
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col-md-12'>
+                    <div id='container'>
+                        <form method='POST' > 
+                            <div class='form-text new' style='display:inline-block;'>
+                            <div style='width: 700px; margin-left: 300px;'>
+                            <h6>Link For</h6>
+                            <input type='text' class='form-control ' value='".$link_for."' readonly /> <br>
+                
+                            <h6>Original Link</h6>
+                            <input type='text' class='form-control ' value='".$original_link."' readonly />
+                            <br>
+                                        <input type='text' id='' style='position:absolute; width: 0px; height: 0px; margin-left: -300px;' name='inn1' value='".$link_for."' />
+                                        <input type='text' id='' style='position:absolute; width: 0px; height: 0px; margin-left: -300px;' name='inn2' value='".$original_link."' />
+                                            
+                                <input type=text class='form-control' name='inn3' style='  padding:20px 5px 20px 180px; font-weight:100em; width:100%;' placeholder='Custom Link Name' id='inn3' required />
+                                <label for='in3' class='static-value' style=' position:absolute;
+                                    color:white;
+                                    left:320px;
+                                    font-weight:bold;
+                                    font-size:1.1em;
+                                    color:#444;
+                                    top:190px;'>https://usict.acm.org/  
+                                </label>
+                            </div> 
+                        </div>
+
+                            <br><br>
+                            <button type='submit' class='btn btn-primary' id='submitBtn' name='reg1' style='margin-left: 300px;' >Check & Confirm</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>";
+    echo "<div style='position:absolute;margin:400px 0px 0px 460px; color:red;'>";
+                echo "<h2>Custom name Already Taken !!</h2>"    ;
+                echo "<h2> &nbsp; Try Another Custom Name..</h2>"   ;
+                echo "</div>";
+    }else{
+        $sql = "SELECT * FROM link";
+        if($result = mysqli_query($link, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_array($result)){
+                        if($row['originalLink']==$original_link){
+                         
+                            $query = "UPDATE link SET shortLink = '". $short_link."'";
+                            $results = mysqli_query($link, $query);
+                            // echo "<br>" . $query;
+                            echo "<script>function copy2(){navigator.clipboard.writeText('".$short_link."');}</script>";
+                    
+    
+                            echo "<div class='wrapper' style='margin:100px 0px 0px 350px;'>";
+                                echo "<div class='container-fluid'>";
+                                    echo "<div class='row'>";
+                                        echo "<div class='col-md-12'>";
+                                            echo "<div class='page-header'></div>";
+                                            echo "<h2>Your Link has been updated ..🥳</h2> ";
+                                            echo "<br>";
+                                        echo "<div>";
+                                    echo "<div>";
+                                echo "<div>";
+                            echo "<div>";
+                            echo "<h4 style='display:inline-block; color:#4e73df; font-weight:bold;'>".$short_link."</h4>";
+                            echo "<button style='margin-left:20px; display:inline-block;' class='btn btn-primary' onclick=copy2()>Copy</button>";
+                            
+                        }
+                    }
+                mysqli_free_result($result);
+            } else{
+                echo "<p class='lead'><em>No Record Found.</em></p>";
+            }
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        }
+    }
+
+      exit();
+}
+
+
+
+
+
   if (isset($_POST['reg'])) {
   	$linkFor = $_POST['in1'];
   	$originalLink = $_POST['in2'];
@@ -66,22 +211,30 @@ $connection = $link;
         
        
         if (mysqli_num_rows($res_o) > 0) {
-                    echo "<div class='wrapper' style='margin:50px 0px 0px 270px;'>";
+                    echo "<div id='appendDiv' class='wrapper' style='margin:50px 0px 0px 270px;'>";
                         echo "<div class='container-fluid'>";
                             echo "<div class='row'>";
                                 echo "<div class='col-md-12'>";
                                     echo "<div class='page-header'></div>";
-                                    echo "<h2>ShortedLink for Given Link has Been Generated previously..</h2> ";
+                                    echo "<h2>ShortedLink for Given Link has Been Generated previously..</h2>";
                                     echo "<br>";
                                 echo "<div>";
                             echo "<div>";
                         echo "<div>";
                     echo "<div>";
             $sql = "SELECT * FROM link";
+            $val_original;
+            $val_short;
+            $val_link_for;
+
             if($result = mysqli_query($link, $sql)){
                 if(mysqli_num_rows($result) > 0){
                         while($row = mysqli_fetch_array($result)){
                             if($row['originalLink']==$originalLink){
+                                $val_original = $row['originalLink'];
+                                $val_short = $row['shortLink'];
+                                $val_link_for = $row['linkFor'];
+
                                 echo "<h6>Original Link</h6>";
                                 echo "<input type='text' class='form-control ' value='".$originalLink."' readonly />";
                                 echo "<br>";
@@ -98,8 +251,42 @@ $connection = $link;
             } else{
                 echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
             }
-            // echo "<br><h2> Wanna change Shorted Link ?</h2>"   ;
-            // echo "</div>";
+            echo "<br><h2> Wanna change Shorted Link ?</h2>";
+            echo "<button id='edit-link' style='position-absolute; margin: -80px 0px 0px 450px;' class='btn btn-primary'>Yes</button>";
+            echo "  <script type='text/javascript'>
+                      document.getElementById('edit-link').addEventListener('click', function(){
+                        //   console.log('aasdfg');
+                        let divTag = document.getElementById('appendDiv');
+                        divTag.innerHTML += `
+                        <div class='wrapper'>
+                        <div class='container-fluid'>
+                            <div class='row'>
+                                <div class='col-md-12'>
+                                    <div id='container'>
+                                        <form method='POST' > 
+                                            <div class='form-text new' style='display:inline-block;'>
+                                            <input type='text' id='' style='position:absolute; width: 0px; height: 0px; margin-left: -300px;' name='inn1' value='".$val_link_for."' />
+                                            <input type='text' id='' style='position:absolute; width: 0px; height: 0px; margin-left: -300px;' name='inn2' value='".$val_original."' />
+                                                <input type=text class='form-control' name='inn3' style='  padding:20px 5px 20px 180px; font-weight:100em; width:100%;' placeholder='Custom Link Name' id='inn3' required />
+                                                <label for='in3' class='static-value' style=' position:absolute;
+                                                    color:white;
+                                                    left:20px;
+                                                    font-weight:bold;
+                                                    font-size:1.1em;
+                                                    color:#444;
+                                                    top:12px;'>https://usict.acm.org/  
+                                                </label>
+                                            </div> 
+                                            <br><br>
+                                            <button type='submit' class='btn btn-primary' id='submitBtn' name='reg1' >Check & Confirm</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+                      });
+                    </script>";
             exit();
         }else{
             if (mysqli_num_rows($res_u) > 0) {
