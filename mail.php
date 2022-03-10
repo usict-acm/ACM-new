13a8c5fe597575e5210b33fb00aab5d77adf66fe
Moulik Agrawal <almoulik@gmail.com>
2022-03-10 21:51:41 +0530

    Logo and pictures changed

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
     function welcomeMail ($userEmail, $userName) {
-        $subject = "Welcome to USICT ACM Student Chapter";
+        $subject = "Welcome to GGSIP University USS ACM Student Chapter";
 
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
