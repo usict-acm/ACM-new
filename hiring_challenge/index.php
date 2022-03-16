eb2d9318b5da94d42a9d17f6291bdffbf231c223
Rithik Sharma <srithik645@gmail.com>
2022-03-16 17:04:29 +0530

    scroll up button

--- hiring_challenge/index.php
+++ hiring_challenge/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link href="https://fonts.googleapis.com/css?family=Vollkorn:400,400i,600,700,900&display=swap" rel="stylesheet" />
-    <link href="../assets/CSS/submission.css" rel="stylesheet" />
+    <!-- <link href="../assets/CSS/submission.css" rel="stylesheet" /> -->
     <link rel="stylesheet" href="style.css" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -675,6 +675,9 @@
                     of ACM USICT chapter.
                 </p>
             </div>
+            <a href="#home" class="scroll-up-btn">
+            <i class="fas fa-arrow-up"></i>
+        </a>
         </div>
     </section>
     <!--************************************************* End of FOOTER **************************************************-->
