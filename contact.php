51008e107f6c802cf441b2025192d360cd353d11
VaibhavArora314 <vaibhavarora0301vishu@gmail.com>
2022-03-19 10:30:37 +0530

    Contact module

---
+++ contact.php
@@ -0,0 +1,12 @@
+<!-- back to top -->
+<div class="scrolltop">
+	<div class="scroll icon">
+        <i class="fa fa-rocket" aria-hidden="true"></i>
+    </div>
+</div>
+<!-- contact us -->
+<div class="contact-bottom-btn">
+	<a href="javascript:showcontact()">
+		<div class="contact-icon"><i class="fas fa-comments" aria-hidden="true"></i></div>
+	</a>
+</div>
--- index.php
+++ index.php
@@ -396,18 +396,10 @@
 	include("footer.php")
 	?>
 	<!--  footer ends -->
-	<!-- back to top -->
-	<div class="scrolltop">
-		<div class="scroll icon"><i class="fa fa-rocket" aria-hidden="true"></i></div>
-	</div>
-	<!-- contact us -->
-	<div class="contact-bottom-btn">
-		<a href="javascript:showcontact()">
-			<div class="contact-icon"><i class="fas fa-comments" aria-hidden="true"></i></div>
-		</a>
-	</div>
-
-
+	<!-- back to top and contact us-->
+	<?php
+	include("contact.php")
+	?>
 
 	<!-- Swiper JS -->
 	<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
