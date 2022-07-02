37f4c51abe549a18913fa511790d9ac443cb221f
Rithik Sharma <srithik645@gmail.com>
2022-07-02 23:27:44 +0530

    event page section-2

--- events.php
+++ events.php
@@ -21,6 +21,8 @@
   <!-- <script src="https://kit.fontawesome.com/d459eda8d9.js" crossorigin="anonymous"></script> -->
   <!-- <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/css/swiper.min.css'> -->
   <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="https://unpkg.com/flickity@2.0/dist/flickity.min.css">
+    <script src="https://unpkg.com/flickity@2.0/dist/flickity.pkgd.min.js"></script>
   <style>
     .minheight{
       min-height: 100vh;
@@ -59,6 +61,65 @@
 
     </section>
     <!--------------------------------------------------------------------- End of Section -1 ---------------------------------------------------------------->
+
+    <!--------------------------------------------------------------------- Start of Section -2 ---------------------------------------------------------------->
+
+    <Section id="section-2">
+        <div class="section-2-heading">
+            <h1>Previous Events</h1>
+        </div>
+        <div class="carousel-wrapper">
+            <div class="carousel" data-flickity>
+                <div class="carousel-cell">
+                    <h3>2019</h3>
+                    <h5 class="more">"The year in which it all started."</h5>
+                    <h4 class="more">8 events</h4>
+                    <img
+                        src="https://images.unsplash.com/photo-1464305795204-6f5bbfc7fb81?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
+                    <div class="line"></div>
+                    <div class="button-2">
+                        <span><button type="button" class="btn btn-primary">See all Events</button></span>
+                    </div>
+                </div>
+                <div class="carousel-cell">
+                    <h3>2020</h3>
+                    <h5 class="more">"Pandemic cannot stop us!"</h5>
+                    <h4 class="more">8 events</h4>
+                    <img
+                        src="https://images.unsplash.com/photo-1464305795204-6f5bbfc7fb81?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
+                    <div class="line"></div>
+                    <div class="button-2">
+                        <span><button type="button" class="btn btn-primary">See all Events</button></span>
+                    </div>
+                </div>
+                <div class="carousel-cell">
+                    <h3>2021</h3>
+                    <h5 class="more">"We grow stronger and the journey is still ongoing."</h5>
+                    <h4 class="more">27 events</h4>
+                    <img
+                        src="https://images.unsplash.com/photo-1464305795204-6f5bbfc7fb81?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
+                    <div class="line"></div>
+                    <div class="button-2">
+                        <span><button type="button" class="btn btn-primary">See all Events</button></span>
+                    </div>
+                </div>
+                <div class="carousel-cell">
+                    <h3>2022</h3>
+                    <h5 class="more">"Mile by mile we hit a milestone!"</h5>
+                    <h4 class="more">12 events</h4>
+                    <img
+                        src="https://images.unsplash.com/photo-1464305795204-6f5bbfc7fb81?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
+                    <div class="line"></div>
+                    <div class="button-2">
+                        <span><button type="button" class="btn btn-primary">See all Events</button></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </Section>
+
+    <!--------------------------------------------------------------------- End of Section -2 ---------------------------------------------------------------->
 
 </section>
 
