5832240321c981f340fbfd18f427e9175723dba5
Ayushi Khandelia <khandeliaayushi08@gmail.com>
2022-07-09 20:34:38 +0530

    bulkdownload

--- admin/adminPanel/faqadmin/adminBulkDown.php
+++ admin/adminPanel/faqadmin/adminBulkDown.php
@@ -1,6 +1,7 @@
 <?php
     include_once '../../blogAdmin/database.php';
     require('fpdf.php');
+    include 'phpqrcode/qrlib.php';
     $database = new Database();
     $conn = $database->connect();
 
@@ -21,31 +22,108 @@
                   //  echo mysqli_num_rows($result);
                     //$row = mysqli_fetch_array($result);
                     //print_r($row);
-                    header('Content-type: image/jpeg');
-                    $font=realpath('arial.ttf');
-                    $image=imagecreatefromjpeg("certi.jpeg");
+                    header('Content-type: image/png');
+                $font=realpath('arial.ttf');
+                //$image;
+                $rank = $row['rank'];
+                if(strlen($rank)>0){
+                    $image=imagecreatefrompng("Certificate.png");
                     $color=imagecolorallocate($image, 51, 51, 102);
+
+
+                    //  if($rank==''){
+                    //     $image=imagecreatefrompng("participation.png");
+                    //     $color=imagecolorallocate($image, 51, 51, 102);
+                    //    //echo"ayushi";
+                    //    }
+
+
                     $date=$row['startDate'];
-                    imagettftext($image, 18, 0, 1025, 710, $color,$font, $date);
+                    imagettftext($image, 25, 0, 1025, 1015, $color,$font, $date);
                     $name=strtoupper($row['nameParticipant']);
                     $uni = $row['uniqueNo'];
                     $course = $row['course'];
                     $enroll = $row['enrollment_no'];
-                    $rank = $row['rank'];
+                    $college = $row['college'];
                     $event = $row['event'];
-                    imagettftext($image, 22, 0, 620, 500, $color,$font, $name);
-                    imagettftext($image, 22, 0, 420, 610, $color,$font, $course);
-                    imagettftext($image, 22, 0, 1010, 610, $color,$font, $enroll);
-                    imagettftext($image, 22, 0, 650, 660, $color,$font, $rank);
-                    imagettftext($image, 22, 0, 850, 660, $color,$font, $event);
-                    
-                    imagejpeg($image,"certificate/download/$uni.jpg");
-                    $pdf=new FPDF();
-                    $pdf->AddPage();
-                    $pdf->Image("certificate/download/$uni.jpg",0,0,210,150);
-                    $pdf->Output("certificate/download/$uni.pdf","F");
-                    imagejpeg($image);
-                    //imagedestroy($image);    
+                    $id = $row['ID'];
+                    $path = 'images/';
+                    //echo $uni;
+                    $file = $path.uniqid().".png";
+                    // $ecc = 'L';
+                    $frame_Size = 1;
+                    $qrtext = "http://localhost/ACM-new/admin/adminPanel/faqadmin/certi_download.php?Sno=".$uni;
+                    QRcode::png($qrtext, $file,'L', 3, $frame_Size);
+                    $qr = imagecreatefrompng($file);
+                    $marge_right = 130;
+                    $marge_bottom = 130;
+                    $sx = imagesx($qr);
+                    $sy = imagesy($qr);
+                    imagettftext($image, 30, 0, 900, 680, $color,$font, $name);
+                    imagettftext($image, 30, 0, 460, 830, $color,$font, $course);
+                    imagettftext($image, 30, 0, 1370, 830, $color,$font, $enroll);
+                    imagettftext($image, 30, 0, 450, 960, $color,$font, $event);
+                    imagettftext($image, 25, 0, 1650, 350, $color,$font, $uni);
+                    imagettftext($image, 30, 0, 900, 900, $color,$font, $college);
+                    imagettftext($image, 30, 0, 1480, 900, $color,$font, $rank);
+                    imagecopy($image, $qr, imagesx($image) - $sx - $marge_right, imagesy($image) - $sy - $marge_bottom, 0, 0, imagesx($qr), imagesy($qr));
+                    unlink($file);
+                    imagepng($image);
+                    imagepng($image,"certificate/download/$id.png");
+                    imagedestroy($image);
+                    //echo "<img width='500' height='350' align='top' alt='' src='certificate/$id.png' />";
+
+                        // $pdf=new FPDF();
+                        // $pdf->AddPage();
+                        // $pdf->Image("certificate/$id.png",0,0,210,150);
+                        // $pdf->Output("certificate/$id.pdf","F");
+                }else{
+
+                    $image=imagecreatefrompng("participation.png");
+                    $color=imagecolorallocate($image, 51, 51, 102);
+                    $date=$row['startDate'];
+                    imagettftext($image, 25, 0, 1025, 1015, $color,$font, $date);
+                    $name=strtoupper($row['nameParticipant']);
+                    $uni = $row['uniqueNo'];
+                    $course = $row['course'];
+                    $enroll = $row['enrollment_no'];
+                    $college = $row['college'];
+                    $event = $row['event'];
+                    $id = $row['ID'];
+                    $path = 'images/';
+                    //echo $uni;
+                    $file = $path.uniqid().".png";
+                    // $ecc = 'L';
+                    $frame_Size = 1;
+                    $qrtext = "http://localhost/ACM-new/admin/adminPanel/faqadmin/certi_download.php?Sno=".$uni;
+                    QRcode::png($qrtext, $file,'L', 3, $frame_Size);
+                    $qr = imagecreatefrompng($file);
+                    $marge_right = 130;
+                    $marge_bottom = 130;
+                    $sx = imagesx($qr);
+                    $sy = imagesy($qr);
+                    imagettftext($image, 30, 0, 900, 680, $color,$font, $name);
+                    imagettftext($image, 30, 0, 460, 830, $color,$font, $course);
+                    imagettftext($image, 30, 0, 1370, 830, $color,$font, $enroll);
+                    imagettftext($image, 30, 0, 450, 960, $color,$font, $event);
+                    imagettftext($image, 25, 0, 1650, 350, $color,$font, $uni);
+                    imagettftext($image, 30, 0, 900, 900, $color,$font, $college);
+                    //imagettftext($image, 30, 0, 1480, 900, $color,$font, $rank);
+                    imagecopy($image, $qr, imagesx($image) - $sx - $marge_right, imagesy($image) - $sy - $marge_bottom, 0, 0, imagesx($qr), imagesy($qr));
+                    unlink($file);
+
+
+                    imagepng($image,"certificate/download/$id.png");
+                    imagepng($image);
+                    //echo"alert('certificate genrated scuusessfully')";
+                    imagedestroy($image);
+
+                    // $pdf=new FPDF();
+                    // $pdf->AddPage();
+                    // $pdf->Image("certificate/$id.png",0,0,210,150);
+                    // $pdf->Output("certificate/$id.pdf","F");
+
+                } 
                 }
             }      
     }
