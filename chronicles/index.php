665fb84f5a0fee9329e43f81bb5492098d767c8f
Kamakshi Nanda <kamakshinanda@Kamakshis-MacBook-Air.local>
2022-08-27 22:25:09 +0530

    API to fetch magazine added.

--- chronicles/index.php
+++ chronicles/index.php
@@ -10,21 +10,20 @@ $db = $database->connect();
 
 $year = $_GET['year'];
 
-$sql = "SELECT * FROM magazine WHERE year = $year";
-
-$stmt = $db->query($sql);
-
-// $stmt->bind_param('s', $year);
-// echo $year;
-// $stmt->execute();
-// echo $year;
-// $result = $stmt->get_result();
-// echo $year;
-// $row = $result->fetch_array();
-// echo $year;
-// echo '<img src="data:image/jpeg;base64,'.base64_encode($row['image']).'"/>';
-// echo $year;
-
+$sql = "SELECT year,magazine FROM magazine WHERE year = ?";
+$stmt = $db->prepare($sql);
+$stmt->bind_param('s', $year);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_array();
+if($row[1] == ''){
+    echo "No magazine exists for this year";
+}
+else{
+    print("<html>");
+    print ('<img src="data:image/jpeg;base64,'.base64_encode($row[1]).'"/>');
+    print("<html/>");
+}
 
 //reference https://stackoverflow.com/questions/20556773/php-display-image-blob-from-mysql
 ?>
