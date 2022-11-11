b17a2fe16d7ecac505976a3cd06cc97ac889fb21
Ayush <ayush4002gupta@gmail.com>
2022-11-11 17:37:49 +0530

    pushing calender

--- dashboard/frontend/src/components/Dashboard/UpcomingEvents.js
+++ dashboard/frontend/src/components/Dashboard/UpcomingEvents.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import { Card, CardBody } from 'reactstrap'
 import { fetchAllEvents } from 'api/event'
 import { Link } from 'react-router-dom'
 import '../../assets/css/dashboard/recentblog.css'
 import moment from 'moment'
-
+import App from 'components/Calender/App'
 const UpcomingEvents = () => {
   const [data, setData] = useState([])
 
@@ -22,28 +22,7 @@ const UpcomingEvents = () => {
 
   return (
     <>
-      {data.length === 0 ? (
-        <div>No Upcoming Events !!</div>
-      ) : (
-        data
-          .map((event) => (
-            <Link to={`/event/${event.eventId}`} key={event.eventId}>
-              <Card className='dashboardCard shadow-lg mb-4'>
-                <CardBody className='dashboardCard__container'>
-                  <div className='dashboardCard__date'>
-                    <h2>{moment(event?.startDate).format('DD MMM, YY')}</h2>
-                  </div>
-                  <div className='dashboardCard__contents'>
-                    <h2 id='bold'>
-                      <span>{event.eventName}</span>
-                    </h2>
-                  </div>
-                </CardBody>
-              </Card>
-            </Link>
-          ))
-          .slice(0, 3)
-      )}
+ <App/>
     </>
   )
 }
