aad9fdfd702e2fe098f81ec43471730fc46eeaa4
apoorvaron <apoorv.aron02@gmail.com>
2022-11-15 12:01:42 +0530

    download certificate & data to db done

--- admin/adminPanel/faqadmin/sample.php
+++ admin/adminPanel/faqadmin/sample.php
@@ -31,11 +31,4 @@ if(mysqli_num_rows($result)>0){
 }
 
 
-
-            //$image=imagecreatefrompng("img.png");
-            
-            
-
-             //imagepng($image, "sample.png");
-//echo "jfkjdfgjlksdfnglkjfngjnfjdnbfdjbnldjfbd";
 ?>
