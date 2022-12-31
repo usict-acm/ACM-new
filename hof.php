82a54eab2f1a8a22dca822b8116da737201d4edf
Pranshu <pranshgupta54@gmail.com>
2022-12-31 19:49:01 +0530

    hof php page

---
+++ hof.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <?php
+      include("head.php")
+    ?>
+     <title>GGSIP University USS ACM Student Chapter</title>
+     <link href="assets/CSS/hof.css" rel="stylesheet" />
+     <link href="assets/CSS/glider.css" rel="stylesheet" />
+     <!-- <link href="assets/CSS/header.css" rel="stylesheet" />
+     <link href="assets/CSS/footer.css" rel="stylesheet" /> -->
+     <link rel="stylesheet" href="./assets/CSS/newStyle.css" />
+
+</head>
+
+<body>
+     <!-- ************************************************Navbar******************************************************************************-->
+     <?php
+      $currentPage = 'hof';
+      include('header.php');
+    ?>
+     <!-- Header Seaction-->
+     <div class="globalContent">
+          <div class="u-overflow-hidden">
+               <header class="header">
+                    <div class="container-lg">
+                         <div class="header-content">
+                              <h1 class="head-1 common-PageTitle header-title">
+                                   <strong>Hall of Fame</strong><br/>
+                              </h1>
+
+                              <p class="common-IntroText header-intro">
+                                   Projects developed under GGSIP University USS ACM Student Chapter
+                                   
+                                   <br><br><br><br><br>
+
+                              </p>
+                         </div>
+                    </div>
+                    <!--Strips Background-->
+                    <div class="common-StripeGrid anchorBottom">
+                         <!-- Background -->
+                         <div class="backgroundContainer">
+                              <div class="grid">
+                                   <div class="background"></div>
+                              </div>
+                         </div>
+                         <!-- Stripes -->
+                         <div class="stripeContainer">
+                              <div class="grid">
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                              </div>
+                         </div>
+                    </div>
+               </header>
+               <!--Introduction Section-->
+               <section class="today section section--padding">
+                    <div class="container-lg">
+                         <div class="today-content">
+                              <div>
+                                   <h3 class="head-3 today-title">INTRODUCTION</h3>
+
+                                   <p class="common-MediumBodyText">
+                                        GGSIP University USS ACM Student Chapter organizes various events, webinars, and workshops to
+                                        enhance the computer and scientific knowledge among the students. Chapter
+                                        members are offered many opportunities, from mentors providing essential
+                                        technical knowledge to information about lucrative internships up for grabs. We
+                                        have conducted over 30 major events and multiple mini-events in the past year,
+                                        with a growing network of more than 500 students.
+                                   </p>
+                              </div>
+                              <!--Img Section-->
+                              <div class="today-chart">
+                                   <div class="chart"></div>
+                              </div>
+                         </div>
+                    </div>
+                    <!--Strips Background-->
+                    <div class="common-StripeGrid">
+                         <!-- Background -->
+                         <div class="backgroundContainer">
+                              <div class="grid">
+                                   <div class="background"></div>
+                              </div>
+                         </div>
+                         <!-- Stripes -->
+                         <div class="stripeContainer">
+                              <div class="grid">
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                                   <div class="stripe"></div>
+                              </div>
+                         </div>
+                    </div>
+               </section>
+          </div>
+
+
+          <!--Different icon section-->
+          <!-- <div class="globalContent"> -->
+          <section class="customers section section--padding">
+               <!--Icon header-->
+               <div class="container-lg">
+                    <div class="section-intro">
+                         <h2 class="head-2 common-SectionTitle">"Creativity is thinking up new things. Innovation is doing new things."</h2>
+                         <p class="common-IntroText">
+                              
+                         </p>
+                    </div>
+               </div>
+               <!--icons-->
+
+               <div class="row d-flex justify-content-center mobile-view">
+                    <div class="collab-card">
+                   <hr class="blue-hr">
+                   <div class="card-1"></div>
+                     <h1 class="collab-heading font-weight-bold">Coursera</h1>
+                     <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo. </p>
+                   </div>
+                    <div class="collab-card">
+                         <hr class="blue-hr">
+                         <div class="card-2"></div>
+                         <h1 class="collab-heading font-weight-bold">Project Name<br></h1>
+                         <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo. </p>
+                    </div>
+                    <div class="collab-card">
+                         <hr class="blue-hr">
+                         <div class="card-3"></div>
+                         <h1 class="collab-heading font-weight-bold">Lenskart</h1>
+                         <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo. </p>
+                    </div>
+                    <div class="collab-card">
+                         <hr class="blue-hr">
+                         <div class="card-4"></div>
+                         <h1 class="collab-heading font-weight-bold">Coding Ninja</h1>
+                         <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo. </p>
+                    </div>
+                    <div class="collab-card">
+                         <hr class="blue-hr">
+                         <div class="card-5"></div>
+                         <h1 class="collab-heading font-weight-bold">CAD DESK </h1>
+                         <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo. </p>
+                    </div>
+               </div>
+               <section class="text-center">
+    <div class="container">
+      <!-- <h2 class="display-4 font-weight-bold my-3">Recent Blogs</h2> -->
+      <div class="row justify-content-center" id="threeBlogsRow"></div>
+      <a href="./collab.php">
+        <button class="btn collab-btn">
+          <span>COLLABORATIONS</span>
+        </button>
+      </a>
+    </div>    
+  </section>
+          </section>
+       <!-- </div> -->
+     <div id="site-wrapper">
+          <div id="site-body">
+               <div id="milestones" class="section2 lazy-bg-loaded">
+                    <div class="wrapper clearfix">
+                         <h2 class="heading">Milestones</h2>
+
+                         <div class="item right">
+                              <div class="marker multiline"></div>
+                              <div class="content">
+                                   <div class="time">2019</div>
+                                   <div>
+                                        <p>Inaugration of GGSIP University USS ACM Student Chapter</p>
+                                        <!-- <p>7 Million Web Pages Created</p> -->
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="item left">
+                              <div class="marker multiline"></div>
+                              <div class="content">
+                                   <div class="time">2020</div>
+                                   <div>
+                                        <p>Growth from 15 members to 500+ members</p>
+
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="item right">
+                              <div class="marker multiline"></div>
+                              <div class="content">
+                                   <div class="time">2021</div>
+                                   <div>
+                                        <p>Global Winners, ACM outstanding website</p>
+
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+               <div id="address" class="section2 lazy-bg-loaded">
+                    <div class="">
+                         <div>
+                              <h2 class="text-white heading text-center header-content">Team</h2>
+                              <div class="grid2c">
+                                   <div class="flex-row-c">
+                                        <div class="img-sec-c">
+                                             <img src="./media/p1.jpeg" alt="">
+                                             <div class="abt-info">
+                                                  <h2 class="name-c">PRANSHU GUPTA</h2>
+                                                  <p>Developer</p>
+                                             </div>
+                                             <a href="https://github.com/pranshugupta54/TechFestWeb" target="_blank"><button class="info-c">More Info</button></a>
+                                        </div>
+                                   </div>
+                                   <div class="flex-row-c">
+                                        <div class="img-sec-c">
+                                             <img src="./media/p2.jpeg" alt="">
+                                             <div class="abt-info">
+                                                  <h2>PRANSHU GUPTA</h2>
+                                                  <p>Developer</p>
+                                             </div>
+                                             <a href="https://github.com/pranshugupta54/TechFestWeb" target="_blank"><button class="info-c">More Info</button></a>
+                                        </div>
+                                   </div>
+                                   <div class="flex-row-c">
+                                        <div class="img-sec-c">
+                                             <img src="./media/p3.jpeg" alt="">
+                                             <div class="abt-info">
+                                                  <h2>PRANSHU GUPTA</h2>
+                                                  <p>Developer</p>
+                                             </div>
+                                             <a href="https://github.com/pranshugupta54/TechFestWeb" target="_blank"><button class="info-c">More Info</button></a>
+                                        </div>
+                                   </div>
+                                   <div class="flex-row-c">
+                                        <div class="img-sec-c">
+                                             <img src="./media/p4.jpeg" alt="">
+                                             <div class="abt-info">
+                                                  <h2>PRANSHU GUPTA</h2>
+                                                  <p>Developer</p>
+                                             </div>
+                                             <a href="https://github.com/pranshugupta54/TechFestWeb" target="_blank"><button class="info-c">More Info</button></a>
+                                        </div>
+                                   </div>
+                                   <div class="flex-row-c">
+                                        <div class="img-sec-c">
+                                             <img src="./media/p5.jpeg" alt="">
+                                             <div class="abt-info">
+                                                  <h2>PRANSHU GUPTA</h2>
+                                                  <p>Developer</p>
+                                             </div>
+                                             <a href="https://github.com/pranshugupta54/TechFestWeb" target="_blank"><button class="info-c">More Info</button></a>
+                                        </div>
+                                   </div>
+                                   <div class="flex-row-c">
+                                        <div class="img-sec-c">
+                                             <img src="./media/p6.png" alt="">
+                                             <div class="abt-info">
+                                                  <h2>PRANSHU GUPTA</h2>
+                                                  <p>Developer</p>
+                                             </div>
+                                             <a href="https://github.com/pranshugupta54/TechFestWeb" target="_blank"><button class="info-c">More Info</button></a>
+                                        </div>
+                                   </div>
+                                   <div class="flex-row-c">
+                                        <div class="img-sec-c">
+                                             <img src="./media/p7.jpeg" alt="">
+                                             <div class="abt-info">
+                                                  <h2>PRANSHU GUPTA</h2>
+                                                  <p>Developer</p>
+                                             </div>
+                                             <a href="https://github.com/pranshugupta54/TechFestWeb" target="_blank"><button class="info-c">More Info</button></a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+ </div>
+ <!-- ****************************************************contact us ***************************** -->
+ <div class="dsph" id="contact">
+		<div class="d-flex justify-content-center mt-md-5">
+
+			<img src="./assets/images/contact_us_img.webp" class="svg-media" alt="" />
+
+			<div class="contactUs">
+				<div class="closebtn">
+					<button class="btn btn-primary s-form-group contact-btn" onclick="closecontact()"><i class="fas fa-times"></i></button>
+				</div>
+				<form class="s-form" name="contact" method="post" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeW8dszRK5ynb6HS51X7fGrfW7su1_8JFL0Tm3hWfbfdpZiCQ/formResponse" onsubmit="return validateform();" >
+					<h2 class="my-4 display-4 fw-bolder text-center">Contact<span class="text-blue"> Us</span></h2>
+					<div class="row form-row">
+						<div class="form-group s-form-group col-md-5">
+							<input type="text" name="name" class="form-control" placeholder="Name *" />
+						</div>
+						<div class="form-group s-form-group col-md-5">
+							<input type="text" name="email" class="form-control" aria-describedby="emailHelp" placeholder="Email *" />
+						</div>
+					</div>
+					<div class="row form-row">
+					<div class="form-group s-form-group col-md-5">
+							<input type="number" name="phone" class="form-control" placeholder="Phone No." pattern="[0-9]+"/>
+						</div>
+						<div class="form-group s-form-group col-md-5">
+							<input type="text" name="college" class="form-control" placeholder="College/ Organization" />
+						</div>
+					</div>
+					<div class="contact-msg">
+					<textarea type="text" name="message" rows="5" placeholder="Message *" class="form-control col-md-11 contact-message"></textarea>
+					</div>
+					<div class="row contact-msg">
+						<button type="submit" class="btn btn-primary s-form-group contact-btn col-md-3 col-sm-2">Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+  <!-- back to top -->
+	<div class="scrolltop">
+		<div class="scroll icon"><i class="fa fa-rocket" aria-hidden="true"></i></div>
+	</div>
+	<!-- contact us -->
+	<div class="contact-bottom-btn">
+		<a href="javascript:showcontact()">
+			<div class="contact-icon"><i class="fas fa-comments" aria-hidden="true"></i></div>
+		</a>
+	</div>
+     <!-- ***********************************************Footer************************************************************************ -->
+
+     <?php
+      include('footer.php');
+    ?>
+     <script src="assets/JS/glider.js"></script>
+     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+     <script src="assets\JS\home_new.js"></script>
+</body>
+
+</html>
