ec1cc7b07630ad28477dce3626fb9ccf306b2226
apoorvaron <apoorv.aron02@gmail.com>
2023-03-01 01:35:47 +0530

    thanku page added

---
+++ forms/thankYou/index_old.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Thank You !!!</title>
+</head>
+<body style="padding-top:90px; background:#f2f2f2" >
+  
+    <div class="jumbotron text-center" >
+      <h1 class="display-3">Thank You!!</h1>
+
+      <span class="lead" style="padding-top:20px; padding-bottom:10px; border-bottom:2px solid black;" ><strong>Please check your email regularly</strong>. </span>
+      <br> <br> We will catch you there.
+      <!-- <p style="margin-top:35px;">
+        Having trouble? <a href="">Contact us</a>
+      </p> -->
+      <p class="lead" style="margin-top:35px;">
+        <a class="btn btn-primary btn-sm" href="/" role="button">Continue to homepage</a>
+      </p>
+    </div>
+</body>
+</html>
+
