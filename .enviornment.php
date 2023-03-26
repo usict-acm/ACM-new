355cb85f28ad8bad7ccaedb8689f00901a056bf4
apoorvaron <apoorv.aron02@gmail.com>
2023-03-26 12:30:55 +0530

    env removed

--- .enviornment.php
+++
@@ -1,7 +0,0 @@
-<?php
-            $env_server = "localhost";
-            $env_username = "root";
-            $env_password = "root";
-            $env_database = "acmbackend";
-            $env_port = "8889";
-?>
