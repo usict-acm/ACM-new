2fa84c292a0ee09702ea9c6215996a8166285ece
modycheif <ankitpandey200154@gmail.com>
2023-04-13 21:40:36 +0530

    Added contributers page

--- hashtech/src/components/Home.js
+++ hashtech/src/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Hero from './Hero'
+import Contributers from './Contributers'
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Contributers /></>
+    
   )
 }
