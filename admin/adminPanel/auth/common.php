c2b0286a0d374b681545f19c817889af01557f75
Sarthak <77429824+sarthakdevil@users.noreply.github.com>
2024-01-21 18:43:01 +0530

    Add files via upload

---
+++ admin/adminPanel/auth/common.php
@@ -0,0 +1,9 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "users2";
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    ?>
