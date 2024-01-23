5560631f2cf3a56798cf93134551bde55ffeba0e
Sarthak <77429824+sarthakdevil@users.noreply.github.com>
2024-01-23 19:51:46 +0530

    Delete admin/adminPanel/auth/common.php

--- admin/adminPanel/auth/common.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "users";
-
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    ?>
